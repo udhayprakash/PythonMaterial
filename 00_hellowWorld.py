cd39fef2342637855cfacd04f320dcc09507843b
Pethakamsetty, Uday Prakash <UdayPrakash.Pethakamsetty@contractor.ca.com>
2017-08-29 21:41:48 +0530

    Added scripts
    Added new scripts

---
+++ 00_hellowWorld.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# shebang line
+
+print 'hello world!'
+
+# python is a dynamic typed language
+a = 12
+b = 34
+
+c = a + b
+print c
+
+type(a)  # until we place print statement, nothing from the script will be
+         # displayed on console
+
+print type(a)
---
+++ 01_ArithmeticOperations2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+"""
+Purpose: Arithmetic Operations
+"""
+
+# compound operators
+#  += -= *= /= %=
+
+myNumber = 123
+print 'myNumber = ', myNumber
+
+myNumber = myNumber + 1
+print 'myNumber = ', myNumber
+
+# In cases, where the same variable is present both the sides, then compound operations are valid
+
+myNumber += 1   # myNumber = myNumber + 1
+print 'myNumber = ', myNumber
+
+myNumber -= 58   # myNumber = myNumber - 58
+print 'myNumber = ', myNumber
+
+myNumber *= 100   # myNumber = myNumber * 100
+print 'myNumber = ', myNumber
+
+myNumber /= 10   # myNumber = myNumber / 10
+print 'myNumber = ', myNumber
+
+myNumber %= 10   # myNumber = myNumber % 10
+print 'myNumber = ', myNumber
+
+print '-----------------------------------------------'
+print 'bitwise Operations'
+# >> <<
+myNewNumber = 4
+print 'myNewNumber =', myNewNumber
+
+result = myNewNumber << 1
+print 'result = ', result
+
+#    8 4 2 1
+#4-  0 1 0 0
+#<<  1 0 0 0
+
+result = 14 >> 2
+print "14 >> 2 = ", result
+#    8 4 2 1
+#14  1 1 1 0
+>>2  0 0 1 1
