18cfe2a9d4f15d9842f7b05d68b6760dd71f4873
Pethakamsetty, Uday Prakash <UdayPrakash.Pethakamsetty@contractor.ca.com>
2017-09-10 19:18:15 +0530

    added Guessing game
    Guessing Game with stupid search

---
+++ algorithms_and_data_structures/01_Guessing_Game_with_Stupid_search.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+"""
+Purpose: Stupid Search.
+
+In a list of numbers, user guesses a number.
+    if it is correct, all is good. Else, next time, he/she will guess among the unguessed numbers.
+"""
+
+from random import randrange
+
+list_of_numbers = range(10, 20)
+
+winning_number = randrange(10, 20)
+# placing this statement within while loop with increase probability of failure
+
+# attempts = len(list_of_numbers)
+
+while True:
+    try:
+        # attempts -= 1
+        guess_number = int(raw_input('Guess a number between 10 and 20:'))
+
+        if guess_number == winning_number:
+            print 'You Guessed correctly'
+            break
+        else:
+            list_of_numbers.remove(guess_number)
+            # print 'Your guess is closer by ', abs(guess_number - winning_number)
+            print 'You guessed larger number' if guess_number > winning_number else 'You guessed lower number'
+
+        if len(list_of_numbers) == 1:  # attempts <= 1:
+            print 'You are STUPID. Use your brain.'
+            print 'Max. attempts completed. The winning number is', list_of_numbers[0]
+
+    except Exception:
+        print 'You STUPID! Guess properly'
+        # all exceptions are catched here, as user should not know the problem
