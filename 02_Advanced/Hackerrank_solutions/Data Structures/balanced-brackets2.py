c42beb5eebd85771a929ef029f7f742f317b3f4e
Pethakamsetty, Uday Prakash <UdayPrakash.Pethakamsetty@contractor.ca.com>
2017-09-26 10:50:23 +0530

    another solution
    Former-commit-id: d38be4c087fea496374694fc2ac539f3a6befecf

---
+++ 02_Advanced/Hackerrank_solutions/Data Structures/balanced-brackets2.py
@@ -0,0 +1,43 @@
+#!/bin/python
+"""
+https://www.hackerrank.com/challenges/balanced-brackets/problem
+"""
+import sys
+
+def isBalanced(s):
+    stack = []
+    for ch in s:
+        if ch in '})]':
+            if not stack:
+                return 'NO'
+            elif ch == ')':
+                if stack[-1] == '(':
+                    stack.remove('(')
+                else:
+                    return 'NO'
+            elif ch == ']':
+                if stack[-1] == '[':
+                    stack.remove('[')
+                else:
+                    return 'NO'
+            elif ch == '}':
+                if stack[-1] == '{':
+                    stack.remove('{')
+                else:
+                    return 'NO'
+            else:
+                stack.append(ch)
+        else:
+            stack.append(ch)
+
+    return 'YES'
+
+if __name__ == "__main__":
+    t = int(raw_input().strip())
+    if 1 <= t <= 10**3:
+        for a0 in xrange(t):
+            s = raw_input().strip()
+            if 1 <= len(s) <= 10**3:
+                result = isBalanced(s)
+                print result
+
