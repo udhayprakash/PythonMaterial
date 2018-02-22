b82ce4be411e6cb03055a2b700d6f48037c07e17
Udhay Prakash Pethakamsetty <praud01@ca.com>
2018-02-22 17:23:27 +0530

    Added recursion examples

---
+++ 00_Basic/05_Functions/recursionEx2.py
@@ -0,0 +1,9 @@
+def toString(n, base):
+    convertString = "0123456789ABCDEF"
+    if n < base:
+        return convertString[n]
+    else:
+        return toString(n/base, base) + convertString[n%base]
+
+print toString(1453, 16)
+print toString(1453, 8)
