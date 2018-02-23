ae3c5b59623b65e693689637e0e51f3c3683185e
Udhay Prakash Pethakamsetty <praud01@ca.com>
2018-02-23 12:01:09 +0530

    Added weather info script

---
+++ 00_Basic/17_Web_Services/01weatherWithOutAuthentication.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Purpose:   Testing https://www.metaweather.com/api/
+"""
+__author__ = "Udhay Prakash Pethakamsetty"
+
+import requests
+from pprint import pprint
+
+
+def location_search(query):
+    try:
+        response = requests.get(r'https://www.metaweather.com//api/location/search/?query=' + query)
+        if response.status_code == 200:
+            # pprint(response.json())
+            for res in response.json():
+                print res['title'] + ' is a ' + res['location_type'] + ' located at ' + res['latt_long']
+        else:
+            print 'Unsuccessful call. Response code is ', response.status_code
+        print
+    except Exception as ex:
+        print 'Unable to execute call. Error is', ex
+
+
+location_search('Hyderabad')
+location_search('India')
+location_search('Anakapalle')  # mid-size town in India
+location_search('san')
+location_search('Alabama')
+location_search('canada')
+location_search('vancouver')
