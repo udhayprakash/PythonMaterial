c4ae2e51cc8a3eb536668a12dd8a9a2a725db777
Udhay Prakash Pethakamsetty <praud01@ca.com>
2018-02-27 18:59:04 +0530

    shutil scripts
    Former-commit-id: 36649923314af1685611910b4cbde964283cd4a0

---
+++ 01_Intermediate/Shutil/shutil-example-1.py
@@ -0,0 +1,15 @@
+# File: shutil-example-1.py
+
+import shutil
+import os
+
+
+# copy function copies a file in pretty much the same way as the Unix cp command.
+
+targetDirectoryPath = "pyDemo/Demo Programs"
+
+for file in os.listdir("."):
+    if os.path.splitext(file)[1] == ".py":
+        print file
+        shutil.copy(file, os.path.join(targetDirectoryPath, file))
+
