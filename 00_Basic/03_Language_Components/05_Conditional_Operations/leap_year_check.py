6d0e4c692c1e8d95c12b5b9ac8bb85e9d0f104c7
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-05-30 10:18:55 +0530

    Added examples in conditions and loops
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 03708e7b1875b5cceb774ce36518a6266d3ce335

---
+++ 00_Basic/03_Language_Components/05_Conditional_Operations/leap_year_check.py
@@ -0,0 +1,17 @@
+# Python program to check if the input year is a leap year or not
+
+#year = 2018
+
+# To get year (integer input) from the user
+year = int(raw_input('year='))
+
+if (year % 4) == 0:
+   if (year % 100) == 0:
+       if (year % 400) == 0:
+           print("{0} is a leap year".format(year))
+       else:
+           print("{0} is not a leap year".format(year))
+   else:
+       print("{0} is a leap year".format(year))
+else:
+   print("{0} is not a leap year".format(year))
