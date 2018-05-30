95d401e5abdd7537438981fb50f54b3545d51973
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-05-30 11:35:57 +0530

    time module sscripts
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 2dd0e79c59f40b6a6c5393c430a0415d9eb38972

---
+++ 00_Basic/17_Web_Services/03_requests/example.py
@@ -0,0 +1,20 @@
+import time
+import requests
+
+def download(url):
+    return requests.get(url)
+
+if __name__ == "__main__":
+
+    urls = [
+        'http://google.com',
+        'http://facebook.com',
+        'http://youtube.com',
+        'http://baidu.com',
+        'http://yahoo.com',
+    ]
+    start = time.time()
+    responses = [download(url) for url in urls]
+    html = [response.text for response in responses]
+    end = time.time()
+    print "Time: %f seconds" % (end - start)
