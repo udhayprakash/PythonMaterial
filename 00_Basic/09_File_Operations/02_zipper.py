d0383df43ac43e317c4f0df509768a58070ba2ec
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-06-02 08:44:20 +0530

    zipper file
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

--- 00_Basic/09_File_Operations/02_zipper.py
+++ 00_Basic/09_File_Operations/02_zipper.py
@@ -3,11 +3,6 @@ from datetime import datetime
 from zipfile import ZipFile
 
 
-# set file name and time of creation
-today = datetime.now()
-dir_name = os.environ.get('TEMP', None)  # update path
-file_name = dir_name + os.sep + 'zipper_' + today.strftime('%Y.%m.%dh%H%M') + '.zip'
-
 
 def zipdir(path, zip):
     for root, dirs, files in os.walk(path):
@@ -15,6 +10,14 @@ def zipdir(path, zip):
             zip.write(os.path.join(root, file))
 
 if __name__ == '__main__':
+    # set file name and time of creation
+    today = datetime.now()
+    dir_name = os.environ.get('TEMP', None)  # update path
+    print 'dir_name', dir_name
+    file_name = dir_name + os.sep + 'zipper_' + today.strftime('%Y.%m.%dh%H%M') + '.zip'
+    print 'file_name', file_name
+
+    # Zipping work
     zipfile = ZipFile(file_name, 'w')
     zipdir(dir_name, zipfile)
     zipfile.close()
