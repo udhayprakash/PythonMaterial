3a5f268cc6d124508ad211c2755eb33325925e30
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-06-03 13:48:21 +0530

    boolean
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

--- 00_Basic/03_Language_Components/03_Boolean_Operations/01_Boolean_Operations.py
+++ 00_Basic/03_Language_Components/03_Boolean_Operations/01_Boolean_Operations.py
@@ -5,34 +5,6 @@ Purpose: Boolean Operations
 
 # True, False
 
-choice = True
-print 'choice = ', choice
-
-true = 'Udhay Prakash'  # NOT RECOMMENDED to use 'true' for variable name
-
-choice = true
-print 'choice = ', choice
-
-choice = False
-print 'choice = ', choice
-
-# Object 
-#  - address location where it is stored - id()
-#  - type(s)
-#  - value(s)
-
-print "id(True) = ", id(True)
-print "id(true) = ", id(true)
-
-print "type(True) =", type(True)
-print "type(true) =", type(true)
-
-print "True = ", True
-print "True * 30 = ", True * 30   # True has a value of one
-
-print "False = ", False
-print "False * 30 = ", False * 30   # False has a value of zero
-
 
 print "True == 1 ", True == 1
 print "True == 3 ", True == 3
@@ -43,36 +15,8 @@ print "False == 0 ", False == 0
 print "False == 3 ", False == 3
 print "False != 3 ", False != 3
 
-
-
-## bool()
-# integer type (int, long and float)
-#       zero     - False 
-#       non-zero - True
-print "bool(12)", bool(12)
-print "bool(-12)", bool(-12)
-print "bool(0)", bool(0)
-
-print "bool(0.00)", bool(0.00)
-print "bool(0.000000001)", bool(0.000000001)
-print "bool(-0.000000001)", bool(-0.000000001)
-
-
-# strings
-#  True - non-empty string 
-#  False - empty string
-print "true", true
-print "bool(true)", bool(true)
-print "bool('')", bool('') # empty string
-print "bool(' ')", bool(' ')  # white-space
-
-# [], (), {}
-print '(1)', bool((1))
-print '(1,)', bool((1,))
-print '[1]', bool([1])
-print '{1}', bool({1})
-print '{1:2}', bool({1:2})
-
-print '()', bool(())
-print '[]', bool([])
-print '{}', bool({})
+# True acts like  1 and 
+# False acts like 0 in all cases, except when coverted 
+# to strings
+print "str(1) != str(True)", str(1) != str(True)
+print "str(0) != str(False)", str(0) != str(False)
---
+++ 00_Basic/03_Language_Components/03_Boolean_Operations/03_Boolean_Operations.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+"""
+Purpose: Boolean Operations
+"""
+## bool()
+# integer type (int, long and float)
+#       zero     - False 
+#       non-zero - True
+print "bool(12)", bool(12)
+print "bool(-12)", bool(-12)
+print "bool(0)", bool(0)
+
+print "bool(0.00)", bool(0.00)
+print "bool(0.000000001)", bool(0.000000001)
+print "bool(-0.000000001)", bool(-0.000000001)
+
+
+# strings
+#  True - non-empty string 
+#  False - empty string
+print "true", true
+print "bool(true)", bool(true)
+print "bool('')", bool('') # empty string
+print "bool(' ')", bool(' ')  # white-space
+
+# [], (), {}
+print '(1)', bool((1))
+print '(1,)', bool((1,))
+print '[1]', bool([1])
+print '{1}', bool({1})
+print '{1:2}', bool({1:2})
+
+print '()', bool(())
+print '[]', bool([])
+print '{}', bool({})
