98b24bcc8fc8347b0d036f4dd44573a7f8504e03
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-06-03 22:09:10 +0530

    named tuples
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: e3783ad54276d46b35294e217474e026c27603b5

---
+++ 00_Basic/04_Collections/02_Tuples/named_tuples.1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+"""
+named tuples provide 
+1. access by index - like normal tuples
+2. access by keynaame - like dictionaries
+3. using getattr()
+"""
+import collections
+ 
+# Declaring namedtuple()
+Student = collections.namedtuple('Student',['name','age','DOB'])
+ 
+# Adding values
+S = Student('Nandini','19','2541997')
+ 
+# Access using index
+print "The Student age using index is : "
+print S[1]
+ 
+# Access using name 
+print "The Student name using keyname is : "
+print S.name
+ 
+# Access using getattr()
+print "The Student DOB using getattr() is : "
+print getattr(S,'DOB')
