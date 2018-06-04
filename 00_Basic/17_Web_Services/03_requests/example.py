ab3b7096c1e33a70225d33ef2d7cf8752eccb547
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-06-04 08:13:23 +0530

    requests
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 033d2705aa9e3f578a623ac5f30c60c0e7aeccfa

--- 00_Basic/17_Web_Services/03_requests/example.py
+++ 00_Basic/17_Web_Services/03_requests/example.py
@@ -1,6 +1,6 @@
 import time
 import requests
-
+import pdb
 def download(url):
     return requests.get(url)
 
@@ -15,6 +15,8 @@ if __name__ == "__main__":
     ]
     start = time.time()
     responses = [download(url) for url in urls]
+    print responses[0]
+    # pdb.set_trace()
     html = [response.text for response in responses]
     end = time.time()
     print "Time: %f seconds" % (end - start)
