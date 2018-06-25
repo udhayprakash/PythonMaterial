6c5d9f698156dd4172f741860cf5753f346b116e
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-06-25 23:13:10 +0530

    added scripts
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ 00_Basic/09_File_Operations/02_non_flat_files/01_csv_files/06_read_csv.py
@@ -0,0 +1,5 @@
+# Import pandas
+import pandas as pd
+
+# Load csv
+df = pd.read_csv("example.csv") 
---
+++ 00_Basic/09_File_Operations/02_non_flat_files/02_xlsx_files/02_read_excelsheets.py
@@ -0,0 +1,42 @@
+# Import `load_workbook` module from `openpyxl`
+from openpyxl import load_workbook
+
+# Load in the workbook
+wb = load_workbook('./test.xlsx')
+
+# Get sheet names
+print(wb.get_sheet_names())
+
+# Get a sheet by name 
+sheet = wb.get_sheet_by_name('Sheet3')
+
+# Print the sheet title 
+sheet.title
+
+# Get currently active sheet
+anotherSheet = wb.active
+
+# Check `anotherSheet` 
+anotherSheet
+
+# Retrieve the value of a certain cell
+sheet['A1'].value
+
+# Select element 'B2' of your sheet 
+c = sheet['B2']
+
+# Retrieve the row number of your element
+c.row
+
+# Retrieve the column letter of your element
+c.column
+
+# Retrieve the coordinates of the cell 
+c.coordinate
+
+# Retrieve cell value 
+sheet.cell(row=1, column=2).value
+
+# Print out values in column 2 
+for i in range(1, 4):
+     print(i, sheet.cell(row=i, column=2).value)
