65cf4938f7a8375a17b882e3404e3ce0d2c2f6ef
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-06-25 23:12:02 +0530

    updated scripts
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: b7814401380dbe224f476dd71f695340ce74532c

--- 00_Basic/17_Web_Services/02_urllib2/get_no_of_people_in_ISS_now.py
+++ 00_Basic/17_Web_Services/02_urllib2/get_no_of_people_in_ISS_now.py
@@ -17,13 +17,14 @@ API
 """
 import json
 import urllib2
-
+from pprint import pprint
 req = urllib2.Request("http://api.open-notify.org/astros.json")
 response = urllib2.urlopen(req)
 
 obj = json.loads(response.read())
 
-print obj['message']
+pprint(obj, indent=2, depth=3)
+
 if obj['message'] == 'success':
     people_count = obj['number']
     print "Currently, total number of people living in ISS now are %d in number" % people_count
