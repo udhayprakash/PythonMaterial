e7d73c3d077febe05a47acb41fa0f52a4f6cd017
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-06-30 21:16:47 +0530

    added scripts
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ 00_Basic/06_Modules/02_os/01_os_module.py
@@ -0,0 +1,17 @@
+import os 
+print os.path.basename("/home/user/temp.txt")  # temp.txt
+print os.path.dirname("/home/user/temp.txt")   # /home/user
+print os.path.abspath('temp.txt')              # /home/user/temp.txt
+print os.path.realpath("/home/user/temp.txt")
+print
+print os.path.splitext("/home/user/temp.txt")  # ('/home/user/temp', '.txt')
+print os.path.splitdrive("/home/user/temp.txt")  # ('', '/home/user/temp.txt')
+print os.path.extsep                            # .
+print os.sep                                    # /
+print 
+
+print os.getcwd()
+
+fd = "GFG.txt"
+os.rename(fd,'New.txt')
+os.rename(fd,'New.txt')
---
+++ 00_Basic/16_Code_Quality/01_doctest/02_doctest.py
@@ -0,0 +1,10 @@
+def foo():
+    """
+    >>> foo()
+    hello ...
+    """
+    print("hello world")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True, optionflags=doctest.ELLIPSIS)
