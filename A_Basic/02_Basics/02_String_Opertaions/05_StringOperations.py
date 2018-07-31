fd79f16648e54c76650bb1ab0b1fdfb1dfcc3240
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-07-31 23:25:23 +0530

    updated scripts
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ A_Basic/02_Basics/02_String_Opertaions/05_StringOperations.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+"""
+Purpose: Demonstration of String Operations
+"""
+
+print '  python Production  '
+print
+print '  python Production  '.strip()
+print '  python Production  '.strip('p')
+print '  python Production  '.strip('p ')
+print '  python Production  '.strip('p thno')
+print
+print '  python Production  '.strip()
+print '  python Production  '.lstrip()
+print '  python Production  '.rstrip()
+
+print ' abcba '.lstrip('a ')
+print ' abcba '.rstrip('a ')
+print
+print 'Python Production'.split(' ')
+print 'Python Production'.split('r')
+print 'Python Production'.split('p')
+print 'Python Production'.split('P')
+print 'Python Production'.rsplit('P')
+print 'Python Production'.split('Prod')
+print
+
+print ''.join(['Python', 'Production'])
+print '@'.join(['Python', 'Production'])
+print '___'.join(['Python', 'Production'])
+print '___'.join('Python Production'.split('P'))
+print 'P'.join('Python Production'.split('P'))
+print 'R'.join('Python Production'.split('P'))
+print ' '.join('Python Production'.split(' '))
+
+print '--------STRING FORMATTING-------'
+print '' % ()
+print '%d' % (12)
+# print '%d'%('12')  # TypeError: %d format: a number is required, not str
+print '%s' % ('12')
+print '%f' % (12.34)
+print '%f' % (12)
+# %d - integer
+# %s - string/char
+# %f - floating-point
+
+print 'lucky number is %d only.' % (786)
+print 'lucky number is %d only.' % 786
+print 'pi value is %f!!!!!!!!!!' % (3.1416)
+print 'my name is %s.' % ('Udhay Prakash')
+
+print 'My name is %s. I am %d old paying a tax of %f'
+print 'My name is %s. I am %d old paying a tax of %f' % ('Udhay', 33, 15.5)
+print 'My name is %r. I am %r old paying a tax of %r' % ('Udhay', 33, 15.5)
+# print 'My name is %s. I am %d old paying a tax of %f'%('Udhay', 33 )
+#   #TypeError: not enough arguments for format string
--- A_Basic/03_Language_Components/02_Logical_Operations/01_Logical_Operations.py
+++ A_Basic/03_Language_Components/02_Logical_Operations/01_Logical_Operations.py
@@ -32,6 +32,7 @@ print "True or True  ", True or True
 print "True or False ", True or False
 print "False or True  ", False or True
 print "False or False ", False or False
+
 print
 print "True     ", True
 print "not True ", not True
