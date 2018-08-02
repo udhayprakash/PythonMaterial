9939984195663833ba475f7997c1907e3724f67a
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-08-02 23:33:08 +0530

    updated scripts
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: d44fe908a59ecca799d3191b0de0978f338c0ad8

--- A_Basic/04_Collections/03_Sets/01_sets_ex.py
+++ A_Basic/04_Collections/03_Sets/01_sets_ex.py
@@ -6,16 +6,34 @@ print 'type(mylist)', type(mylist)
 print 'len(mylist)', len(mylist)
 print 'mylist', mylist
 print
+
 myset = {11, 22, 11, 44, 22, 11}
 print 'type(myset)', type(myset)
 print 'len(myset)', len(myset)
 print 'myset', myset
 print
+
+
+# sets can't be indexed
+# print myset[0]  # TypeError: 'set' object does not support indexing
+
 filtered_list = list(set(mylist))
 print 'type(filtered_list)', type(filtered_list)
 print 'len(filtered_list)', len(filtered_list)
 print 'filtered_list:', filtered_list
 
+
+'''
+int()
+long()
+float()
+
+list()
+tuple()
+set()
+dict()
+
+'''
 
 
 
