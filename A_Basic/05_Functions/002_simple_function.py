4b4118819f87f2572da1f37a3c485fefbd9dd5a7
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-08-07 23:24:07 +0530

    added scripts
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: a3f1245fb3b56e809a1a958f49fa6bc3ac1df6b9

---
+++ A_Basic/05_Functions/002_simple_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""
+Purpose: Functions Demo
+
+    Function with one input argument and no return value
+"""
+
+# Function Definition
+def hello(name):
+    print "Hello " + name
+
+
+# Function Call 
+# hello() # TypeError: hello() takes exactly 1 argument (0 given)
+
+hello('Python')
---
+++ A_Basic/05_Functions/006_simple_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""
+Purpose: Functions Demo
+
+    Function with two arguments and two return values
+"""
+
+# Function Definition
+def hello():
+    return 123, 45
+
+# Function Call 
+result = hello()
+print "type(result)=", type(result)
+print "result      =", result
