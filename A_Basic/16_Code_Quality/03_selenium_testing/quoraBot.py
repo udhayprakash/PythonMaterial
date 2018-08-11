496cae67b78af86770dcfce9a1603400c2d5d435
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-08-11 10:25:41 +0530

    added scripts
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ A_Basic/16_Code_Quality/03_selenium_testing/quoraBot.py
@@ -0,0 +1,37 @@
+#Check out pythonhowto.com for more code tutorials !!
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+
+driver = webdriver.Chrome()
+
+email="" #Your Password Here
+passy="" #Your Password Here
+driver.get("http://quora.com")
+
+soup = BeautifulSoup(driver.page_source,"lxml")
+forms = soup.find_all(class_="form_column")
+
+#Find Login Css Selectors Here
+for x in forms:
+    try:
+        data=x.find("input")["name"]
+        if data == "email":
+            email_css = "#" + x.find("input")["id"]
+        if data == "password":
+            password_css = "#" + x.find("input")["id"]
+    except:
+        pass
+    try:
+        data=x.find("input")["value"]
+        if data=="Login":
+            login_css = "#" + x.find("input")["id"]
+    except:
+        pass
+
+
+driver.find_element_by_css_selector(email_css).send_keys(email)
+driver.find_element_by_css_selector(password_css).send_keys(passy)
+time.sleep(2)
+driver.find_element_by_css_selector(login_css).click()
+time.sleep(2)
