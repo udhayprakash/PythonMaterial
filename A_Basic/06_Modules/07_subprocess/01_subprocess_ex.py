a2d33ca613cfe26c4bf6a22b81de614dbec6556e
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-08-13 23:45:59 +0530

    updated scripts; removed pyc files
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

--- A_Basic/06_Modules/07_subprocess/01_subprocess_ex.py
+++ A_Basic/06_Modules/07_subprocess/01_subprocess_ex.py
@@ -1,20 +1,20 @@
 from subprocess import call, Popen
-from sys import platform,exit
-import os 
+from sys import platform, exit
+import os
 
 # subprocess.Popen is more general than subprocess.call.
 # Popen doesn't block, allowing you to interact with the 
 # process while it's running, or continue with other things 
 # in your Python program; whereas call does block.
 
-if platform in ['linux','linux2', 'darwin']:
+if platform in ['linux', 'linux2', 'darwin']:
     os.system('ls -l /usr/bin/python')
     call('ls -l /usr/bin/python', shell=True)
     result = Popen('ls -l /usr/bin/python', shell=True)
     print 'result==============\n', result
 elif platform == 'win32':
-    os.system('dir /x C:\Python27')
-    call('dir /x C:\Python27', shell=True)
+    print os.system('dir /x C:\Python27')
+    print call('dir /x C:\Python27', shell=True)
     result = Popen('dir /x C:\Python27', shell=True)
     print 'result==============\n', result
 else:
