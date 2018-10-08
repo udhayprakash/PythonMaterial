86a11e28a5db163de58b9a0dfe9d069091285a4f
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-10-08 23:01:55 +0530

    updated scripts
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 72ceb0b012d66f1d696bf30d449cddc4430f078f

--- A_Basic/03_Language_Components/05_Conditional_Operations/01_lotteryTicket.py
+++ A_Basic/03_Language_Components/05_Conditional_Operations/01_lotteryTicket.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python
 """
 Purpose:
-lottery ticket
-  winning ticket = 'Ajvhvj87689'
-  ask the user to input the ticket number in run-time and evaluate
+    Monday to Friday  : - 9 am to 6 pm
+    Saturday          : - 9 am to 1 pm
+    Sunday            : - Holiday
 """
 __author__ = 'Python Tutor'
 
-winningTicket = 'Ajvhvj87689'
+day_of_the_week = raw_input('Enter the day of the week:').lower()
+print "day_of_the_week:", day_of_the_week
 
+if (day_of_the_week == 'monday'
+        or day_of_the_week == 'tuesday'
+        or day_of_the_week == 'wednesday'
+        or day_of_the_week == 'thursday'
+        or day_of_the_week == 'friday'):
+
+    print "Office Timings: 9 am to 6 pm"
+
+elif day_of_the_week == 'saturday':
+    print "Office Timings: 9 am to 1 pm"
+elif day_of_the_week == 'saturday':
+    print "SUNDAY is HOLIDAY!!!!!!"
+else:
+    print "Please enter the correct weekname!"
+
+#     :
+#     print "Office Timings: 9 am to 6 pm"
+# elif day_of_the_week == 'tuesday':
+# print "Office Timings: 9 am to 6 pm"
+# elif day_of_the_week == 'wednesday':
+# print "Office Timings: 9 am to 6 pm"
+# elif day_of_the_week == 'thursday':
+# print "Office Timings: 9 am to 6 pm"
+# elif day_of_the_week == 'friday':
--- A_Basic/03_Language_Components/05_Conditional_Operations/electricity_bill_generation.py
+++ A_Basic/03_Language_Components/05_Conditional_Operations/electricity_bill_generation.py
@@ -1,6 +1,23 @@
 #!python -u
 """
-Purpose:Electricity Board Current Bill Slab rates 
+Purpose:Electricity Board Current Bill Slab rates
+electricity bill
+
+	0 till 60   ===> 1.25
+	60 till 100  ===> 2.00
+	100 till 150 ===> 4.00
+	150 till 250 ===> 7.00
+	250 +        ===> 10
+
+electricity cess : 2%
+GST              : 7%
+
+discount         : -1.11%
+
+units consumed : 357
+
+
+	60 + 40 + 50 + 100 + 107
 """
 __author__ = 'Python Tutor'
 
@@ -8,9 +25,9 @@ name = str(raw_input("Please enter your name: "))
 number = int(raw_input("Please enter service number?: "))
 units = int(raw_input("How many units consumed?: "))
 
-if units < 50:
+if units < 60:
     amt = 3 * units
-elif units > 50 and units < 100:
+elif units > 60 and units < 100:
     amt = ((5 * (units - 50)) + 150)
 elif units > 100 and units < 150:
     amt = ((7 * (units - 100)) + 400)
