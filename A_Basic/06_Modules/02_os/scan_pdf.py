0a5b88b059fb83492c02038eee2dac98bd74c838
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-10-15 22:53:23 +0530

    added script
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

--- A_Basic/06_Modules/02_os/scan_pdf.py
+++ A_Basic/06_Modules/02_os/scan_pdf.py
@@ -1,40 +1,42 @@
-import os, os.path
+import os
+import os.path
 import re
 
+
 # This is the first implementation
-def print_pdf (arg, dir, files):
-   for file in files:
-      path = os.path.join (dir, file)
-      path = os.path.normcase (path)
-      if re.search (r".*\.pdf", path):
-         print path
+def print_pdf(arg, dir, files):
+    for file in files:
+        path = os.path.join(dir, file)
+        path = os.path.normcase(path)
+        if re.search(r".*\.pdf", path):
+            print path
 
 
 # This is the second implementation, which skips
 # files with spaces in their names.
-def print_pdf (arg, dir, files):
-   for file in files:
-      path = os.path.join (dir, file)
-      path = os.path.normcase (path)
-      if not re.search (r".*\.pdf", path): continue
-      if re.search (r" ", path): continue
+def print_pdf(arg, dir, files):
+    for file in files:
+        path = os.path.join(dir, file)
+        path = os.path.normcase(path)
+        if not re.search(r".*\.pdf", path): continue
+        if re.search(r" ", path): continue
 
-      print path
+        print path
 
 
 # This is the third implementation, which
 # skips over files that have the letter ".hu" in the path.
-def print_pdf (arg, dir, files):
-   for file in files:
-      path = os.path.join (dir, file)
-      path = os.path.normcase (path)
-      if not re.search (r".*\.pdf", path): continue
-      if re.search (r".\.hu", path): continue
+def print_pdf(arg, dir, files):
+    for file in files:
+        path = os.path.join(dir, file)
+        path = os.path.normcase(path)
+        if not re.search(r".*\.pdf", path): continue
+        if re.search(r".\.hu", path): continue
 
-      print path
+        print path
 
-os.path.walk ('.', print_pdf, 0)
+
+os.path.walk('.', print_pdf, 0)
 
 # This will default to the last definition in the file
-os.path.walk ('.', print_pdf, 0)
-
+os.path.walk('.', print_pdf, 0)
