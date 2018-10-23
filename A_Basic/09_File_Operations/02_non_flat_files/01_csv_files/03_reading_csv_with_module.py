342f3c690b24f6e3762fc1f8b1cd9e73aaebdf7c
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-10-23 23:09:07 +0530

    updated scripts
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 201c29b54fe10830f68b04fd5558c955b30f40e7

--- A_Basic/09_File_Operations/02_non_flat_files/01_csv_files/03_reading_csv_with_module.py
+++ A_Basic/09_File_Operations/02_non_flat_files/01_csv_files/03_reading_csv_with_module.py
@@ -6,6 +6,9 @@ with open('mydata.csv', 'rb') as csv_fh:
     data = csv.reader(csv_fh, delimiter=',')
     print data, type(data)
 
+    # for index, eachline in enumerate(data):
+    #     print index
+
     for loopnumber, eachline in enumerate(data):
         if not loopnumber:
             continue
