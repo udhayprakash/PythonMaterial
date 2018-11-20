e80f71905a09df32177efc1c2382d9d32a22e41f
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-11-20 21:34:30 +0530

    updated scripts
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 6ddfd5ad9ad2230292cb7df9acc22a4ab5bde3e9

--- A_Basic/08_Exceptions/assert_demo.py
+++ A_Basic/08_Exceptions/assert_demo.py
@@ -3,24 +3,24 @@ def first_assert_demo():
     # Demonstrate the use of assert()
     large = 1000
     string = "This is a string"
-    float = 1.0
+    float_value = 1.0
     broken_int = "This should have been an int"
     
     assert large > 500
     assert type(string) == type("")
-    assert type(float) != type(1)
+    assert type(float_value) != type(1)
     assert type(broken_int) == type(4)
 
 def second_assert_demo():
     # Demonstrate the use of assert()
     large = 1000
     string = "This is a string"
-    float = 1.0
+    float_value = 1.0
     broken_int = "This should have been an int"
     
     assert large > 500
     assert type(string) == type("")
-    assert type(float) != type(1)
+    assert type(float_value) != type(1)
 
     try: 
         assert type(broken_int) == type(4), "broken_int is broken"
