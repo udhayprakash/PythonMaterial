3db08f7dd69da36720944ffd93f8ebb79585f2d7
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2018-12-18 06:48:18 +0530

    updated scripts
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 8887641ba05279f1d8abce8c9a230cab53454da2

--- A_Basic/07_Modules/01_sys/argfile.py
+++ A_Basic/07_Modules/01_sys/argfile.py
@@ -1,6 +1,21 @@
+# import time
+# for i in range(90):
+#     time.sleep(1)  # 1sec 
+# name = raw_input('name=')
+# print 'Entered named is', name
+
+
 import sys
 
+print __file__
+print sys.argv
+
+if len(sys.argv) < 2:
+    print 'Enter name'
+    print __file__, 'name'
+    sys.exit(0)
+    
 name = sys.argv[1]
-handle = open(name, 'r')
-text = handle.read()
-print name, 'is', len(text), 'bytes'
+print 'Entered named is', name
+
+
