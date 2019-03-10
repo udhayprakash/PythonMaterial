7fee246be041695a2cb6d32fc10ccf50a607e238
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-03-10 15:11:23 +0530

    removed a project
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

--- others/B_Intermediate/02_django/Titanic/Passengers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from passengers import views
-
-urlpatterns = [
-    path('json-example/', views.json_example, name='json_example'),
-    path('json-example/data/', views.chart_data, name='chart_data'),
-]
