e733cc8a2efbe20f2fccf4b9f16832256cfa62e3
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-03-15 21:24:29 +0530

    os module
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 5322d9f569f5cab4b05fa0fe6cbe24e907b10796

--- python3/10_Modules/01_sys/sys_module.py
+++ python3/10_Modules/01_sys/sys_module.py
@@ -16,24 +16,27 @@ for i in (sys.stdin, sys.stdout, sys.stderr):
     print(i)
 
 print('Hello world!')
-sys.stdout.write('Hello world!')
+sys.stdout.write('Hello world!\n')
 # sys.modules.keys()
 
 
-print('sys.path', sys.path)
+print('sys.path', type(sys.path))
+
+for each_path in sys.path:
+    print(each_path)
+
+# import udhay
+user_input = sys.stdin.readline()
+print("Input : " + user_input)
 
 
-# user_input = sys.stdin.readline()
-# print("Input : " + user_input)
+print('sys.copyright', sys.copyright)
 
 
-# print('sys.copyright', sys.copyright)
-
-
-# # This python sys module method returns the count for references to an object where it is used. Python keeps track of this value, as, when this value reaches 0 in a program, the memory for this variable is cleaned up. 
-# print('sys.getrefcount(0)', sys.getrefcount(0))
-# name = 'Udhay'
-# print('sys.getrefcount(name)', sys.getrefcount(name))
-# print('sys.getrefcount(None)', sys.getrefcount(None))
+# This python sys module method returns the count for references to an object where it is used. Python keeps track of this value, as, when this value reaches 0 in a program, the memory for this variable is cleaned up. 
+print('sys.getrefcount(0)', sys.getrefcount(0))
+name = 'Udhay'
+print('sys.getrefcount(name)', sys.getrefcount(name))
+print('sys.getrefcount(None)', sys.getrefcount(None))
 
 
