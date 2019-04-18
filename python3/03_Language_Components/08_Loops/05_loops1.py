01a8947a2b4eb37258006d443a50111cbacc68fb
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-04-18 07:36:41 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: b44ed09b571f950690fdb5c4273d9a174f957d42

---
+++ python3/03_Language_Components/08_Loops/05_loops1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+"""
+Purpose: 
+	break     - breaks the complete loop
+	continue  - skip the current loop
+	pass      - will do nothing. it is like a todo
+	sys.exit  - will exit the script execution
+"""
+students = ['akram', 'trusha', 'bhavana', 'jaya', 'chaitra']
+
+
+print('break importance ==========')
+for each_student in students:
+    if each_student == 'bhavana':
+        break
+    print(each_student)
+
+print('continue importance ==========')
+for each_student in students:
+    if each_student == 'bhavana':
+        continue
+    print(each_student)
+
+print('pass importance ==========')
+for each_student in students:
+    if each_student == 'bhavana':
+        pass
+    print(each_student)
+
+import sys
+print('sys.exit importance ==========')
+for each_student in students:
+    if each_student == 'bhavana':
+        sys.exit(0)
+    print(each_student)
+
+print('-------next statement')
