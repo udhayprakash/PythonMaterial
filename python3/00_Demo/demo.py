baca929c93fce115c2cf83c6935056c00970464e
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-04-19 07:41:57 +0530

    update
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 682e0246c7ebcf630d22209ecee5ca6ec096af1b

---
+++ python3/00_Demo/demo.py
@@ -0,0 +1,19 @@
+print('Hello world')
+
+
+num1 = 123
+num2 = 345
+add_result = num1 + num2 
+print(add_result)
+
+
+def hello_world(name):
+    return "Hello " + name 
+
+print(hello_world('Udhay'))
+
+
+class MyClass:
+    pass 
+
+my_instance = MyClass()
