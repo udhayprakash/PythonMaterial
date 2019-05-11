3b6160258b902a361888fea52e3e393265cdf205
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-05-11 07:20:25 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: d92627932178912c0209500aa402cd795b9ea46b

--- python3/10_Modules/04_argparse/argparsedemo.1.py
+++ python3/10_Modules/04_argparse/argparsedemo.1.py
@@ -3,30 +3,38 @@
 Purpose: importance and usage of argparse
 """
 
-# Method 1: hard- coding
+
+# # Method 1: hard- coding
 # user_name = 'udhay'
 # password = 'udhay@123'
 # server_name = 'issadsad.mydomain.in'
 
+
+# # Method 2: input() - run time 
+# user_name = input('ENter username:')
+# password = input('ENter password:')
+# server_name = input('ENter server_name:')
+
+
+# # Method 3: sys.argv
 # import sys
-# entered_args = sys.argv
-# # user_name = entered_args[1]
-# # password = entered_args[2]
-# # server_name = entered_args[3]
+# print('sys.argv = ', sys.argv)
 
-# # unpacking 
-# user_name, password, server_name = sys.argv[1:]
+# user_name = sys.argv[1]
+# password = sys.argv[2]
+# server_name = sys.argv[3]
 
+# Method 4: argparse
 import argparse
 
 parser = argparse.ArgumentParser(
     description="Details to login to server")
 parser.add_argument('-u', '--username',
-                    help='user name',
+                    help='login user name',
                     type=str, 
                     required=True)
 parser.add_argument('-p', '--password',
-                    help='password',
+                    help='login password',
                     type=str, 
                     required=True)
 parser.add_argument('-s', '--servername',
@@ -40,6 +48,8 @@ args = parser.parse_args()
 user_name = args.username
 password = args.password
 server_name = args.servername
+
+
 
 print(f'''
 The server login details are:
