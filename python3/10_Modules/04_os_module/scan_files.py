2f64d40e02dd40e918602fffacbec99d8888e790
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-05-13 09:37:59 +0530

    scanning files
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 60b000d31dafc0594575ec3fd50980002f8f02a5

---
+++ python3/10_Modules/04_os_module/scan_files.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+"""
+Purpose: scan files
+"""
+import os
+search_path = r'C:\Users\udhayPrakash\Desktop\PythonMaterial\python3'
+print('current dir:', os.getcwd())
+# print('dir content:\n')
+# for each in os.listdir(search_path):
+#     print(each)
+
+print(os.walk(search_path))
+# dir_path, dirs_in_pth, files_in_path
+
+for each_dir, folders, files in os.walk(search_path):
+    for each_file in files:
+        file_etn = os.path.splitext(each_file)[1]
+        if file_etn == '.py':
+            print(each_file)
