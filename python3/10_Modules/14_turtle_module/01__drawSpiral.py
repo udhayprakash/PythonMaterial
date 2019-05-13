cdef35d53a208d06bed1738ed04df335918e07f9
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-05-13 07:32:35 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 0f266ca4d668270c4a79d549a7f8ccf4a5ae0ca3

--- python3/10_Modules/14_turtle_module/01__drawSpiral.py
+++ python3/10_Modules/14_turtle_module/01__drawSpiral.py
@@ -6,9 +6,9 @@ myWin = turtle.Screen()
 
 def drawSpiral(myTurtle, lineLen):
     if lineLen > 0:
-        myTurtle.forward(lineLen)
+        myTurt le.forward(lineLen)
         myTurtle.right(90)
-        drawSpiral(myTurtle, lineLen - 5)
+        drawSpiral(myTurtle,lineLen - 5)
 
 
 drawSpiral(myTurtle, 100)
--- python3/10_Modules/test__puwinauto__calc.py
+++
@@ -1,6 +0,0 @@
-from subprocess import Popen
-from pywinauto import Desktop
-
-Popen('calc.exe', shell=True)
-dlg = Desktop(backend="uia").Calculator
-dlg.wait('visible')
