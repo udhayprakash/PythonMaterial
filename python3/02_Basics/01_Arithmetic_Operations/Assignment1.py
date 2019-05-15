46963797aa5a163c7b9718bdb7a73d434f66a6df
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-05-15 23:36:06 +0530

    assignment solution
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: f41feba24d36e19804f1bc83872705e368e5bce7

---
+++ python3/02_Basics/01_Arithmetic_Operations/Assignment1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+"""
+Purpose: 
+hair oil  67/1 ltr  ==> qty: 3 ltr 
+apples    12.3/1 dozen  ==> 8 dozen
+mangos    15 /1 piece   ==> 32 pieces
+dozen = 12
+"""
+# cost
+cost_of_hair_oil =  67 #  ltr 
+cost_of_apples = 12.3  # dozen
+cost_of_mango = 15 # 1 piece 
+
+# quantity 
+qty_of_hair_oil = 3 # ltrs
+qty_of_apples = 8 # dozen 
+qty_of_mangos = 32 # pieces
+
+# price = cost * quantity
+sp_hair_oil = cost_of_hair_oil * qty_of_hair_oil
+sp_apples = cost_of_apples * qty_of_apples
+sp_mangos = cost_of_mango * qty_of_mangos
+
+# print('sp_hair_oil')
+# print(sp_hair_oil)
+# print(sp_hair_oil2)
+
+total_sp = sp_hair_oil + sp_apples + sp_mangos
+# print('total_sp =')
+# print(total_sp)
+
+print('total_sp =', total_sp)
+
+# assignment 2 
+# discount = 17.35 % on hair oil 
+# discount = 10.12 % on others 
+
+# GST = 15.99 % 
+
