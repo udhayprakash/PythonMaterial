67d21f47b58e83c30b5b3ba2d6e7555c0a0d08f0
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-05-18 14:06:45 +0530

    sys.stdi stdout
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 24bc448e5a67ccf2a0faf616f189c01d047fe80d

---
+++ python3/10_Modules/02_sys/readingRawInput_using_sys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+'''
+https://www.hackerrank.com/challenges/python-raw-input
+'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from sys import stdin, stdout
+s = stdin.read()#r"How many chickens does it take to cross the road?")
+
+if 1 <= len(s) <= 500:
+    stdout.write(s)
