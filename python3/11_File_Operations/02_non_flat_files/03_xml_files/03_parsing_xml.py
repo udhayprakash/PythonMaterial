25ec9594930c6d46e8b96ba995cd90abe8d1231e
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-05-19 12:42:09 +0530

    xml parsing
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: d0e2153cdf59eb4a8bd87ff8f97a58abad1108b0

---
+++ python3/11_File_Operations/02_non_flat_files/03_xml_files/03_parsing_xml.py
@@ -0,0 +1,15 @@
+"""
+XML parsing: ElementTree (etree) provides a Python-based API for parsing/generating
+"""
+import pprint
+from xml.etree.ElementTree import parse
+
+tree = parse('books.xml')
+
+mapping = {}
+for B in tree.findall('book'):
+    isbn = B.attrib['isbn']
+    for T in B.findall('title'):
+        mapping[isbn] = T.text
+
+pprint.pprint(mapping)
