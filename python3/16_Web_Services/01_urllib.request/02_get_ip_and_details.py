8267177c40a63bb8a2a050b0af4a96b85a18a988
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-06-28 23:33:30 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 36fa43f076ecacf2b70ce05b95f522688650de04

--- python3/16_Web_Services/01_urllib.request/02_get_ip_and_details.py
+++ python3/16_Web_Services/01_urllib.request/02_get_ip_and_details.py
@@ -4,8 +4,8 @@
 GET https://ipapi.co/{format}/
 """
 import os
-import urllib.request, urllib.parse, urllib.error
-
+import urllib.request
+import json
 
 def print_n_write_response(url):
     connection = urllib.request.urlopen(url)
@@ -19,7 +19,11 @@ def write_response_to_file(data, extension_name):
     if not os.path.exists(dirname):
         os.mkdir(dirname)
     with open(dirname + os.sep + 'response.' + extension_name, 'w') as f:
-        f.write(str(data, 'utf-8'))
+        # f.write(str(data, 'utf-8'))
+        # To save in pretty print format 
+        f.write(
+            json.dumps(json.loads(data), indent=4, sort_keys=True)
+            )
         f.close()
 
 
@@ -30,33 +34,33 @@ if __name__ == '__main__':
     print_n_write_response(URL)
     print()
 
-    print('-' * 20)
-    print('JSONP response')
-    URL1 = 'https://ipapi.co/jsonp'
-    print_n_write_response(URL1)
-    print()
+    # print('-' * 20)
+    # print('JSONP response')
+    # URL1 = 'https://ipapi.co/jsonp'
+    # print_n_write_response(URL1)
+    # print()
 
-    print('-' * 20)
-    print('XML response')
-    URL2 = 'https://ipapi.co/xml'
-    print_n_write_response(URL2)
-    print()
+    # print('-' * 20)
+    # print('XML response')
+    # URL2 = 'https://ipapi.co/xml'
+    # print_n_write_response(URL2)
+    # print()
 
-    print('-' * 20)
-    print('CSV response')
-    URL3 = 'https://ipapi.co/csv'
-    print_n_write_response(URL3)
-    print()
+    # print('-' * 20)
+    # print('CSV response')
+    # URL3 = 'https://ipapi.co/csv'
+    # print_n_write_response(URL3)
+    # print()
 
-    print('-' * 20)
-    print('YAML response')
-    URL4 = 'https://ipapi.co/yaml'
-    print_n_write_response(URL4)
-    print()
+    # print('-' * 20)
+    # print('YAML response')
+    # URL4 = 'https://ipapi.co/yaml'
+    # print_n_write_response(URL4)
+    # print()
 
 
-    # # print('-' * 20)
-    # # print('HTML response')
-    # # URL4 = 'https://www.google.co.in/search?q=someting&oq=someting&aqs=chrome..69i57.1510j0j1&sourceid=chrome&ie=UTF-8'
-    # # print_n_write_response(URL4)
-    # # print()
+    # print('-' * 20)
+    # print('HTML response')
+    # URL4 = 'https://ipapi.co/'
+    # print_n_write_response(URL4)
+    # print()
