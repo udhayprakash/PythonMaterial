85bf4aa915388da382392511a77c4d8e2a8c5937
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-06-29 23:26:00 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: d5a20cb1cb4df479e520602f7ac0bbbc4e3f3014

--- python3/10_Modules/02_sys/gettingCommandlineValues1.py
+++ python3/10_Modules/02_sys/gettingCommandlineValues1.py
@@ -9,33 +9,31 @@ import sys
 # for i in  dir(sys):
 #     print i
 #
-#
 # print '-' * 80
 #
 # # Run time value
 # word = raw_input('Enter some word:')
 #
-# print 'You entered:'+ word
-#
+# print('You entered:'+ word)
 
 # sys.argv
 
-print "sys.argv     :", sys.argv
-print "len(sys.argv):", len(sys.argv)
+print("sys.argv     :", sys.argv)
+print("len(sys.argv):", len(sys.argv))
 
 winningTicket = '123Alpha'
 
 if len(sys.argv) >= 2:
     userEnteredTicket = sys.argv[1]
 else:
-    print 'Please enter the ticket number\n'
-    print 'USAGE:'
-    print '      python gettingCommandlineValues.py 231asdas'
-    print '__file__ :', __file__
-    print '      python', __file__, '231sads'
+    print('Please enter the ticket number\n')
+    print('USAGE:')
+    print('      python gettingCommandlineValues.py 231asdas')
+    print('__file__ :', __file__)
+    print('      python', __file__, '231sads')
     sys.exit(1)
 
 if winningTicket == userEnteredTicket:
-    print 'You won the lottery'
+    print('You won the lottery')
 else:
-    print 'Try again!'
+    print('Try again!')
