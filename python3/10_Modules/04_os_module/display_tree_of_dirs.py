db80646e558ab72db494fd2e82ec14813baed4c7
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-07-04 18:57:32 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 0462e5c0a2b7519fd87c9d72d6f569097650412a

---
+++ python3/10_Modules/04_os_module/display_tree_of_dirs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+"""
+Purpose: To display the tree strcuture of directories only , till three levels
+
+test
+   sub1
+   sub2
+       subsub1
+
+"""
+import os
+
+MAX_DEPTH = 3 # levels 
+
+given_path = r'C:\Python27\etc' #input('Enter the path:')
+print(given_path)
+
+def display_folders(_path, _depth):
+    if _depth != MAX_DEPTH:
+        _depth += 1
+        files_n_flders = os.listdir(_path)
+        for each in files_n_flders:
+            if os.path.isdir(os.path.join(_path, each)):
+                print('  ' * _depth, each)
+                display_folders(os.path.join(_path, each), _depth)
+
+display_folders(given_path, 0)
+
