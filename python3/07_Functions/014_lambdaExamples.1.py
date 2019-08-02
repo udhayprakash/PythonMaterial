883921a40b1174fe40b48fdaf4fa57764b2ea2c8
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-08-02 23:29:02 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 571ef3491231ed69222fa63a660a8f5560237493

--- python3/07_Functions/014_lambdaExamples.1.py
+++ python3/07_Functions/014_lambdaExamples.1.py
@@ -6,28 +6,38 @@ __author__ = 'udhay prakash'
 def double(x):
     return x * 2
 
-# functions are treated as first class object
+print(double(12))  # 24
 
-print(double(12))
+dbl = lambda x: x*2
+print(dbl(12))
+print((lambda x:x*2)(12))
+#############################################
 
-print('range(6)             ', list(range(6)))
+def binomial_expression(n):
+    return n**2 - 2*n + 1
+
+print(binomial_expression(9))
+print((lambda n: n**2 - 2*n + 1)(9))
+##############################################
+print('range(6) ', list(range(6)))
 
 new_list = []
 for i in range(6):
     new_list.append(double(i))
 print(new_list)
 
+new_list = []
+for i in range(6):
+    new_list.append((lambda x:x*2)(i))
+print(new_list)
+
 # map(function, iterable1, iterable2, ..)
 # iterable objects - str, list, tuple, set, dictionary
-print('map(double, range(6))', list(map(double, list(range(6)))))  # applying function for a list
-
-db = lambda x: x * 2
-print(db(12))
-print('map(db, range(6))    ', list(map(db, list(range(6)))))  # applying lambda for a list
-print('map(lambda x: x * 2, range(6))', [x * 2 for x in range(6)])  # applying lambda for a list
+print('map(double, range(6))       :', list(map(double, range(6))))  # applying function for a list
+print('map(lambda x: x*2, range(6)):', list(map(lambda x: x*2, range(6))))  # applying function for a list
 
 print('-' * 50)
-
+#######################################
 def sme_operation(m, n):
     return m ** 2 + m * n - 1
 
@@ -37,7 +47,7 @@ db = lambda m, n: m ** 2 + m * n - 1
 print(db(12, 21))
 
 print('-' * 50)
-
+#############################################
 def addition(m, n):
     return m + n
 
@@ -47,8 +57,14 @@ addn = lambda p, q: p + q
 
 print(addn(23, 10))
 
-print(list(map(addition, list(range(6)), list(range(6, 0, -1)))))  # applying function for a list
-print(list(map(addn, list(range(6)), list(range(6, 0, -1)))))  # applying lambda for a list
+# [1, 2, 3] + [3, 4, 5] => [1, 2, 3, 3, 4, 5]
+# [1, 2, 3] , [3, 4, 5] => [4 , 6, 8]
+print(list(map(addition, [1, 2, 3], [3, 4, 5])))
+print(list(map(addn, [1, 2, 3], [3, 4, 5])))
+
+# [1, 2, 3] , [3, 4, 5] => [(1, 3), (2, 4), (3, 5)]
+print(list(map(lambda x,y:(x,y), [1, 2, 3], [3, 4, 5])))
+
 
 print('-' * 80)
 print('with expr')
@@ -59,11 +75,11 @@ print([x[0] for x in [(1, 2), (3, 4), (5, 6)]])
 print([x[1] for x in [(1, 2), (3, 4), (5, 6)]])
 print()
 
-mydict = {'a': 7, 'b': 8, 'c': 9}
-print([a for a in mydict])
-print([a for a in list(mydict.keys())])
-print([a for a in list(mydict.values())])
-print([a[1] for a in list(mydict.items())])
+# mydict = {'a': 7, 'b': 8, 'c': 9}
+# print([a for a in mydict])
+# print([a for a in list(mydict.keys())])
+# print([a for a in list(mydict.values())])
+# print([a[1] for a in list(mydict.items())])
 
 numbers = (12, 11, 12, 15, 20)
 print('numbers', numbers)
@@ -71,8 +87,8 @@ numbers_sorted = sorted(numbers)
 print(type(numbers_sorted), numbers_sorted)
 print('numbers', numbers)
 
-numbers_sorted = sorted(numbers, reverse=True)
-print(type(numbers_sorted), numbers_sorted)
+numbers_sorted1 = sorted(numbers_sorted, reverse=True)
+print(numbers_sorted, numbers_sorted1)
 
 numbers = [12, 11, 12, 15, 20]
 numbers.sort()
@@ -80,11 +96,3 @@ print(type(numbers), numbers)
 numbers.sort(reverse=True)
 print(type(numbers), numbers)
 
-mydict = {'a': 87, 'b': 8, 'c': 19}
-print([x for x in list(mydict.items())])
-
-print(sorted(list(mydict.items()), key=lambda x: x[0]))
-print(dict(sorted(list(mydict.items()), key=lambda x: x[1])))
-print(dict(sorted(list(mydict.items()), key=lambda x: x[1],reverse=True)))
-
-print([x for x in sorted(list(mydict.items()), key=lambda x: x[1])])
--- python3/07_Functions/015_higher_order_functions.py
+++ python3/07_Functions/015_higher_order_functions.py
@@ -16,20 +16,20 @@ print()
 
 print(list(
         zip(
-                (1, 2, 3, 4),
-                (11, 22, 33, 44),
+                (1,   2,   3,   4),
+                (11,  22,  33,  44),
                 (111, 222, 333, 444),
         )
 ))
 
-print(map(None, group1, group2))
-print(dict(map(None, group1, group2)))
+# print(list(map(None, group1, group2)))
+print(list(map(lambda x,y:(x,y), group1, group2)))
 
-print(list(map(my_mapper, group1, group2)))
-print(dict(list(map(my_mapper, group1, group2))))
+# print(list(map(my_mapper, group1, group2)))
+# print(dict(list(map(my_mapper, group1, group2))))
 
-print(list(map(lambda x, y: {x:y}, group1, group2)))
-print(dict(list(map(lambda x, y: {x:y}, group1, group2))))
+# print(list(map(lambda x, y: {x:y}, group1, group2)))
+# print(dict(list(map(lambda x, y: {x:y}, group1, group2))))
 
 print('-'* 25)
 
@@ -46,39 +46,39 @@ for each_num in range(-4,6):
     result.append(check_positive(each_num))
 print(result)
 
-##### using map()
-print(list(map(check_positive, range(-4, 6))))
+# ##### using map()
+# print(list(map(check_positive, range(-4, 6))))
 
-#### Traditionally
-result = []
-for each_num in range(-4,6):
-    if check_positive(each_num):
-        result.append(each_num)
-print(result)
+# #### Traditionally
+# result = []
+# for each_num in range(-4,6):
+#     if check_positive(each_num):
+#         result.append(each_num)
+# print(result)
 
-#### using filter
-print(list(map(check_positive,    range(-4, 6))))
-print(list(filter(check_positive, range(-4, 6))))
+# #### using filter
+# print(list(map(check_positive,    range(-4, 6))))
+# print(list(filter(check_positive, range(-4, 6))))
 
-print('----using comprehensions=========')
-print([each_num for each_num in range(-4,6)])  
-print([each_num>=0 for each_num in range(-4,6)])  # map
-print([each_num for each_num in range(-4,6) if each_num>=0])  # filter
+# print('----using comprehensions=========')
+# print([each_num for each_num in range(-4,6)])  
+# print([each_num>=0 for each_num in range(-4,6)])  # map
+# print([each_num for each_num in range(-4,6) if each_num>=0])  # filter
 
-def double(x):
-        return 2 * x
+# def double(x):
+#         return 2 * x
 
-print('-----using lambda================== anonymous function')
-print([2*x for x in range(-4, 6)])
-print(list(map(double, range(-4, 6))))
-print(list(map(lambda x:x*2, range(-4, 6))))
+# print('-----using lambda================== anonymous function')
+# print([2*x for x in range(-4, 6)])
+# print(list(map(double, range(-4, 6))))
+# print(list(map(lambda x:x*2, range(-4, 6))))
 
-print([num*2 for num in range(-4, 6) if num])
-print(list(map(double, range(-4, 6))))
-print(list(filter(double, range(-4, 6))))
-print(list(filter(lambda x:x*2>=3, range(-4, 6))))
+# print([num*2 for num in range(-4, 6) if num])
+# print(list(map(double, range(-4, 6))))
+# print(list(filter(double, range(-4, 6))))
+# print(list(filter(lambda x:x*2>=3, range(-4, 6))))
 
-# display all the even numbers between -5 & 17
-print([each_num for each_num in range(-5, 17) if each_num%2==0])
-print(list(map(lambda p:p%2==0, range(-5, 17))))
-print(list(filter(lambda p:p%2==0, range(-5, 17))))
+# # display all the even numbers between -5 & 17
+# print([each_num for each_num in range(-5, 17) if each_num%2==0])
+# print(list(map(lambda p:p%2==0, range(-5, 17))))
+# print(list(filter(lambda p:p%2==0, range(-5, 17))))
