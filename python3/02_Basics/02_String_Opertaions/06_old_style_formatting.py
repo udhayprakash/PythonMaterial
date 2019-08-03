b42b911e2db2cccbb23835d3496514fb1ecd65f4
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-08-03 18:14:30 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 32c19c1a9e50907726419b4c34a46bbae42a5b4d

--- python3/02_Basics/02_String_Opertaions/06_old_style_formatting.py
+++ python3/02_Basics/02_String_Opertaions/06_old_style_formatting.py
@@ -2,21 +2,62 @@
 """
 Purpose: String formatting
         OLD STYLE string formatting
+%d - integer
+%i - integer
+
+%c - single char
+%s - string/char
+
+%f - floating-point
+%F - floating-point
+
+%o - octal 
+%x - hexadecimal lowercase 
+%X - hexadecimal uppercase
 """
 
 print('' % ())
 print('%d' % (12))
-# print '%d'%('12')  # TypeError: %d format: a number is required, not str
+print('%i' % (12))
+# print('%d'%('12'))  # TypeError: %d format: a number is required, not str
 
 print()
 print('%s' % ('12'))
 print('%f' % (12.34))
 print('%f' % (12))
 print('%s' % (True))
-# %d - integer
-# %s - string/char
-# %f - floating-point
 
+print()
+#%o - octal 
+print('%o' % (12), oct(12))
+#%x - hexadecimal lowercase 
+print('%x' % (12), hex(12))
+#%X - hexadecimal uppercase 
+print('%X' % (12), hex(12))
+
+print('%s' % ('12'))
+print('%c' % ('1'))
+
+print()
+print('%f' % (12.34)) # 12.340000
+print('%F' % (12.34)) # 12.340000
+
+print()
+print('%g' % (12.34)) # 12.34
+print('%G' % (12.34)) # 12.34
+
+print()
+print('%e' % (12.34)) # 12.34
+print('%E' % (12.34)) # 12.34
+
+print()
+import math 
+print('pi', math.pi)     # 3.141592653589793
+print('%d'% math.pi)     # 3
+print('%f'% math.pi)     # 3.141593
+print('%9f'% math.pi)    #  3.141593
+print('%9.3f'% math.pi)  #     3.142
+###########################################################
 print()
 print('lucky number is %d only.' % (786))
 print('lucky number is %d only.' % 786)
