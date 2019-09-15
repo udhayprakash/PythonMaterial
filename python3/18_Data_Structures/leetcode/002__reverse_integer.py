99074f39e5829c8a6cf3a2cfa727a08ec4cb5999
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-09-15 21:49:51 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 9cf6598e9da4213cfae09edf984dcc2f5370374e

---
+++ python3/18_Data_Structures/leetcode/002__reverse_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+"""
+Purpose: 
+    https://leetcode.com/problems/reverse-integer/
+
+"""
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x < 0:
+            val = -1 *int(str(-1 * x)[::-1].lstrip('0'))
+        elif x == 0:
+            val = 0
+        else:
+            val = int(str(x)[::-1].lstrip('0'))
+            
+        return val if pow(-2, 31) < val < pow(2, 31) - 1 else 0
+
+s = Solution()
+assert s.reverse(123) == 321
+assert s.reverse(-123) == -321
+assert s.reverse(120) == 21
+assert s.reverse(1534236469) == 0
