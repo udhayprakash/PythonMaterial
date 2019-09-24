3bb9caff03831e94f4c41f2c01afb9ce22cb988c
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-09-24 08:58:11 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 2edebc6217a159c67ca3cf9d4cb0d064312385b9

---
+++ python3/16_Web_Services/03_web_scrapping/webScraping4.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+"""
+Purpose: scraping
+"""
+import requests 
+import lxml.html
+
+res = requests.get('https://pydata.org/nyc2018/schedule/')
+print(res.status_code)
+print(res.text)
+tree = lxml.html.fromstring(res.text)
+for tr in tree.xpath('//span[@class="speaker"]'):
+    name = tr.xpath('text()')
+    url = tr.xpath('@href')
+
+    print(name)
+    print(url)
