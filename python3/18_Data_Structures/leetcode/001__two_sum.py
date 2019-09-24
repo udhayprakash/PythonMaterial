8ab0f61a8642e7f32c4189145eb2ba16c5977df0
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-09-24 23:46:30 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 6f7d58a7f4e28363f57b47c871aa0ec265c1a94f

--- python3/18_Data_Structures/leetcode/001__two_sum.py
+++ python3/18_Data_Structures/leetcode/001__two_sum.py
@@ -18,13 +18,30 @@ class Solution:
             for next_num in nums[nums.index(ech_num)+1:]:
                 if ech_num + next_num == target:
                     return [nums.index(ech_num), nums.index(next_num, nums.index(ech_num)+1)]
-
+    
+    def twoSum(self, nums, target):
+        """
+        input : nums: List[int], target: int
+        output: List[int]
+        """
+        seen = {}
+        for index, num in enumerate(nums):
+            other = target - num
+            if other in seen:
+                return [seen[other], index]
+            else:
+                seen[num] = index
+        return []
+    
 s = Solution()
 # s.twoSum(eval(input()), 9)
 
 assert s.twoSum([2, 7, 11, 15], 9) == [0, 1]
 assert s.twoSum([3,2,4], 6) == [1,2]
 assert s.twoSum([3,3], 6) == [0,1]
+assert s.twoSum([3,2,3], 6) == [0,2]
+assert s.twoSum([0,4,3,0], 0) == [0,3]
+assert s.twoSum([-3,4,3,90], 0) == [0, 2]
 '''
 2, 7, 11, 15
 
@@ -37,7 +54,3 @@ assert s.twoSum([3,3], 6) == [0,1]
 
 11, 15
 '''
-
-
-
-
