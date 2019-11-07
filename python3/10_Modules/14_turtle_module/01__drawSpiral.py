8d5c0e8b46bd81f2d9824b49b99ebaeaae6f2298
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-11-07 23:18:33 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: d5d1cd45b63d636abde8e2ca7b3f166161d6d0e4

--- python3/10_Modules/14_turtle_module/01__drawSpiral.py
+++ python3/10_Modules/14_turtle_module/01__drawSpiral.py
@@ -1,15 +1,15 @@
 import turtle
 
-myTurtle = turtle.Turtle()
+my_turtle = turtle.Turtle()
 myWin = turtle.Screen()
 
 
-def drawSpiral(myTurtle, lineLen):
-    if lineLen > 0:
-        myTurtle.forward(lineLen)
-        myTurtle.right(90)
-        drawSpiral(myTurtle, lineLen - 5)
+def drawSpiral(_my_turtle, line_length):
+    if line_length > 0:
+        _my_turtle.forward(line_length)
+        _my_turtle.right(90)
+        drawSpiral(_my_turtle, line_length - 5)
 
 
-drawSpiral(myTurtle, 100)
+drawSpiral(my_turtle, 100)
 myWin.exitonclick()
