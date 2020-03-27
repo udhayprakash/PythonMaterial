fd11b5757799d6960a3012cf60905ceee0cf8eb4
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2020-03-27 08:12:20 +0530

    memory handler
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 4ea70f9665e8eaaf9f7a65b6c56a1634799493f8

--- python3/12_Logging/using_memory_handler.py
+++ python3/12_Logging/using_memory_handler.py
@@ -1,31 +1,51 @@
-import logging, logging.handlers, atexit, sys
+#!/usr/bin/python
+"""
+Purpose: Using Memory Handler
+"""
+import atexit
+import logging.handlers
+import os
+import sys
 
-filename = 'mylogfile.txt'
-logLevel = logging.DEBUG
+LOG_LEVEL = logging.DEBUG
 
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-streamhandler = logging.StreamHandler(sys.stderr)
-streamhandler.setLevel(logLevel)
-streamhandler.setFormatter(formatter)
-memoryhandler = logging.handlers.MemoryHandler(
-    capacity=1024 * 100,
+formatter = logging.Formatter('%(asctime)s - %(levelname)8s - %(message)s')
+
+# Step 1: Create handlers
+stream_handler = logging.StreamHandler(sys.stderr)
+stream_handler.setLevel(LOG_LEVEL)
+stream_handler.setFormatter(formatter)
+
+memory_handler = logging.handlers.MemoryHandler(
+    capacity=1024 * 1,  # 1 kb
     flushLevel=logging.ERROR,
-    target=streamhandler
+    target=stream_handler
 )
 
-filehandler = logging.FileHandler(filename)
-filehandler.setLevel(logLevel)
-filehandler.setFormatter(formatter)
+file_handler = logging.FileHandler(os.path.splitext(__file__)[0] + '.log')
+file_handler.setLevel(LOG_LEVEL)
+file_handler.setFormatter(formatter)
+
+# Step 2: Create logger object
 logger = logging.getLogger()
-logger.setLevel(logLevel)
-logger.addHandler(memoryhandler)
-logger.addHandler(filehandler)
+logger.setLevel(LOG_LEVEL)
+
+# Step 3: Add handlers to the logger object
+logger.addHandler(memory_handler)
+logger.addHandler(file_handler)
 
 
 def flush():
-    memoryhandler.flush()
+    memory_handler.flush()
 
 
 atexit.register(flush)
-logging.debug("Logger has Initialized")
-sys.stderr.write("I'd like this printed on the console first\n")
+loop_count = 0
+while loop_count <= 100000:
+    logger.debug(f'This is {loop_count:6} log')
+    logger.info(f'This is {loop_count:6} log')
+    logger.warning(f'This is {loop_count:6} log')
+    logger.error(f'This is {loop_count:6} log')
+    logger.critical(f'This is {loop_count:6} log')
+    loop_count += 1
+
