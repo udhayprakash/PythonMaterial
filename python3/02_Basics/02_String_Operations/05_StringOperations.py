74c5582614b56b62c6f0e643adb097267f5c6a69
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2020-04-08 21:00:55 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 85d81fd96f1f08bac079a236ecda7f80e9a433e5

--- python3/02_Basics/02_String_Operations/05_StringOperations.py
+++ python3/02_Basics/02_String_Operations/05_StringOperations.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 """
-Purpose: Demonstration of String Operations
+Purpose: String attributes
 """
 
 print('  python Production  ')
@@ -17,50 +17,76 @@ print('  python Production  '.rstrip())
 print('  python Production  '.lstrip('p thnoy'))
 print('  python Production  '.rstrip('p thnoy'))
 
-print(' abcba '.lstrip('a '))
-print(' abcba '.rstrip('a '))
-print()
-# How to convert a string to a list
-print('Python Production'.split())
-print('Python Production'.split(' '))
-print('Python Production'.split('r'))
-print('Python Production'.split('t'))
+print('00001231231230'.lstrip('0'))
+'123'.zfill(3)  # '123'
+'123'.zfill(4)  # '0123'
+'123'.zfill(7)  # '0000123'
 
-print('Python Production'.split('p'))
-print('Python Production'.split('P'))
-print('Python Production'.split('n'))
-print('Python Production'.rsplit('P'))
-print('Python Production'.split('Prod'))
-# print('Python Production'.split('')) # ValueError: empty separator
-print(''.split(' '))
-print()
 # How to convert a string to a list
-print(list('Python Production'))
+print('Python Production'.split())  # ['Python', 'Production']
+print('Python Production'.split(' '))  # ['Python', 'Production']
+print('Python Production'.split('r'))  # ['Python P', 'oduction']
+print('Python Production'.split('t'))  # ['Py', 'hon Produc', 'ion']
+print()
 
+# Question: what is the type of result, if splitting substring is
+#          not present
+print('Python Production'.split('p'))  # ['Python Production']
+print('Python Production'.split('P'))  # ['', 'ython ', 'roduction']
+print('Python Production'.split('n'))  # ['Pytho', ' Productio', '']
+print('Python Production'.rsplit('n'))  # ['Pytho', ' Productio', '']
+
+print('Python Production'.split('Prod'))  # ['Python ', 'uction']
+print('Python Production'.split('Prdo'))  # ['Python Production']
+
+# print('Python Production'.split(''))  # ValueError: empty separator
+
+# Question:
+print(''.split(' '))  # ['']
+
+print()
+print('1201201301201'.split('0'))  # ['12', '12', '13', '12', '1']
+print('1201201301201'.split('0', maxsplit=0))  # ['1201201301201']
+print('1201201301201'.split('0', maxsplit=1))  # ['12', '1201301201']
+print('1201201301201'.rsplit('0', maxsplit=1))  # ['12012013012', '1']
+
+print('1201201301201'.split('0', 3))  # ['12', '12', '13', '1201']
+print('1201201301201'.rsplit('0', 3))  # ['12012', '13', '12', '1']
+
+# How to convert a string to a list
+print(list('1201201301201'))  # ['1', '2', '0', '1', '2', '0', '1', '3', '0', '1', '2', '0', '1']
+
+print(tuple('1201201301201'))  # ('1', '2', '0', '1', '2', '0', '1', '3', '0', '1', '2', '0', '1')
+print(set('1201201301201'))  # {'2', '1', '0', '3'}
+
+# Question:
 '1,2,3'.split(',')  # ['1', '2', '3']
-'1,2,3'.split(',', maxsplit=1)  # ['1', '2,3']
-'1,2,3'.rsplit(',', maxsplit=1)  # ['1', '2,3']
 '1,2,,3,'.split(',')  # ['1', '2', '', '3', '']
 
 # default is with white space
 '1 2 3'.split()  # ['1', '2', '3']
-'1 2 3'.split(maxsplit=1)  # ['1', '2 3']
 '   1   2   3   '.split()  # ['1', '2', '3']
 
-''.split()  # ['']
-
 print()
-# HOw to convert list of strings to a single string
-print(''.join(['Python', 'Production', 'language']))
-print('@'.join(['Python', 'Production', 'language']))
-print('___'.join(['Python', 'Production', 'language']))
+# Question: HOw to convert list of strings to a single string
+print('Python' + 'Production' + 'Language')
+print('Python' + ' ' + 'Production' + ' ' + 'Language')
+print('Python' + '-' + 'Production' + '-' + 'Language')
+
+# ''.join('Python', 'Production', 'Language')
+# TypeError: join() takes exactly one argument (3 given)
+
+print(''.join(['Python', 'Production', 'Language']))
+print(' '.join(['Python', 'Production', 'Language']))
+print('-'.join(['Python', 'Production', 'Language']))
+print('@'.join(['Python', 'Production', 'Language']))
 
 print()
 print('___'.join('Python Production'.split('P')))
 print('P'.join('Python Production'.split('P')))
-print('R'.join('Python Production'.split('P')))
-print(' '.join('Python Production'.split(' ')))
+print('R'.join('Python Production'.split('P')))  # Rython Rroduction
+print()
 
-print('Python Production'.replace('P', 'R'))
-print('Python Production'.replace('Prod', 'cat'))
-print('Python Production'.replace('Prod1', 'cat'))
+print('Python Production'.replace('P', 'R'))  # Rython Rroduction
+print('Python Production'.replace('Prod', 'cat'))  # Python catuction
+print('Python Production'.replace('Prod1', 'cat'))  # Python Production
--- python3/02_Basics/02_String_Operations/06_old_style_formatting.py
+++ python3/02_Basics/02_String_Operations/06_old_style_formatting.py
@@ -1,93 +1,111 @@
 #!/usr/bin/python
 """
-Purpose: String formatting
-        OLD STYLE string formatting
-%d - integer
-%i - integer
+Purpose: String Operations
+    Types
+        1. Old Style formatting
+        2. New Style formatting
+        3. F-strings
 
-%c - single char
-%s - string/char
+OLD-Style formatting
+    %d - integer
+    %i - integer
 
-%f - floating-point
-%F - floating-point
+    %c - single char
+    %s - string/char
 
-%o - octal 
-%x - hexadecimal lowercase 
-%X - hexadecimal uppercase
+    %f - floating-point
+    %F - floating-point
+
+    %o - octal
+    %x - hexadecimal lowercase
+    %X - hexadecimal uppercase
 """
-
 print('' % ())
-print('NUmber is %d')
-print('NUmber is %d' % (12))
-print('NUmber is %i' % (12))
-# print('NUmber is %d'%('12'))  # TypeError: %d format: a number is required, not str
+print('My lucky number is %d')
+print('My lucky number is %d' % (12))
+print('My lucky number is %d' % 12)
+print('My lucky number is %d' % 12.34)  # int(12.34)
+# print('My lucky number is %d' % '12')
+# TypeError: %d format: a My lucky number is required, not str
 
 print()
-print('NUmber is %s' % ('12'))
-print('NUmber is %f' % (12.34))
-print('NUmber is %f' % (12))
+print('My lucky number is %s' % '12')
+print('My lucky number is %f' % 12.34)
+print('My lucky number is %f' % 12)
 
 print()  # str()
-print('NUmber is %s' % (True))
-print('NUmber is %s' % (1))
-print('NUmber is %s' % (13.4))
-print('NUmber is %s' % (133224234))
-print('NUmber is %s' % ("python"))
+print('My lucky number is %s' % True)
+print('My lucky number is %s' % None)
+print('My lucky number is %s' % 1)
+print('My lucky number is %s' % 13.4)
+print('My lucky number is %s' % 133224234)
+print('My lucky number is %s' % "python")
 
 print()  # repr()
-print('NUmber is %r' % (True))
-print('NUmber is %r' % (1))
-print('NUmber is %r' % (13.4))
-print('NUmber is %r' % (133224234))
-print('NUmber is %r' % ("python"))
+print('My lucky number is %r' % True)
+print('My lucky number is %r' % None)
+print('My lucky number is %r' % 1)
+print('My lucky number is %r' % 13.4)
+print('My lucky number is %r' % 133224234)
+print('My lucky number is %r' % "python")
 
 print()
 # %o - octal
-print('%o' % (12), oct(12))
+print('%o' % 12, oct(12))
 # %x - hexadecimal lowercase
-print('%x' % (12), hex(12))
-# %X - hexadecimal uppercase
-print('%X' % (12), hex(12))
-
-print('%s' % ('12'))
-print('%c' % ('1'))
+print('%x' % 12, hex(12))
+# %b - binary
+# print('%b' % (12), bin(12))
 
 print()
-print('%f' % (12.34))  # 12.340000
-print('%F' % (12.34))  # 12.340000
+print('%s' % '12')
+print('%c' % '1')
+
+print()  # float()  -- 6 digits post decimal
+print('%f' % 12.34)  # 12.340000
+print('%F' % 12.34)  # 12.340000
+print('%F' % 12.34223123123123)  # 12.342231
 
 print()
-print('%g' % (12.34))  # 12.34
-print('%G' % (12.34))  # 12.34
+print('%g' % 12.34)  # 12.34
+print('%G' % 12.34)  # 12.34
+print('%G' % 12.34223123123123)  # 12.3422
 
 print()
-print('%e' % (12.34))  # 1.234000e+01
-print('%E' % (12.34))  # 1.234000E+01
+print('%e' % 12.34)  # 1.234000e+01
+print('%E' % 12.34)  # 1.234000E+01
+print('%E' % 12.34223123123123)  # 1.234223E+01
 
 print()
 import math
 
-print(math.pi)          # 3.141592653589793
-print('%d' % math.pi)   # 3
-print('%f' % math.pi)   # 3.141593
-print('%9f' % math.pi)  #  3.141593
-print('%9.3f' % math.pi)#     3.142
+print(math.pi)  # 3.141592653589793
+print('%d' % math.pi)  # 3
+print('%f' % math.pi)  # 3.141593
+print('%9f' % math.pi)  # 3.141593
+print('%9.3f' % math.pi)  # 3.142
+
 ###################################################
 print()
-print('lucky number is %d only.' % (786))
-print('lucky number is %d only.' % 786)
-print('pi value is %f!!!!!!!!!!' % (3.1416))
+print('lucky My lucky number is %d only.' % 786)
+print('pi value is %f!!!!!!!!!!' % 3.1416)
 print('my name is %s - %s.' % ('Udhay', 'Prakash'))
 
 print()
 print('My name is %s. I am %d old paying a tax of %f')
 print('My name is %s. I am %d old paying a tax of %f' % ('Udhay', 99, 15.5))
-
 # print('My name is %s. I am %d old paying a tax of %f' % ('Udhay', '99', 15.5))
+# TypeError: %d format: a My lucky number is required, not str
+
+
 print('My name is %r. I am %r old paying a tax of %r' % ('Udhay', '78', 15.5))
 print('My name is %s. I am %s old paying a tax of %s' % ('Udhay', '78', 15.5))
 
-# print('My name is %s. I am %d old paying a tax of %f'%('Udhay', 33 ))
+# print('My name is %s. I am %s old paying a tax of %s' % ('Udhay', 15.5))
 # TypeError: not enough arguments for format string
 
-print('%(language)s has %(number)03d quote types.' % {'language': "Python", "number": 2})
+print('%(language)s has %(My lucky number)03d quote types.' % {'language': "Python", "My lucky number": 2})
+
+print()
+print('2'.zfill(3))
+print('%03d' % 2)
