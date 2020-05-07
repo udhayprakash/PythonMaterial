9b4ebecb36eac9fa9be067c33e942d6b2bb41117
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2020-05-08 00:59:38 +0530

    csv file operations
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: cfbfc1e3096538057223f8c751cf71fd5de76b06

---
+++ python3/11_File_Operations/02_structured_files/04_csv/j_read_csv_numeric_data.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+"""
+Purpose: Read numeric data from csv files 
+
+By default, data is read as a list of strings. 
+If we specify the QUOTE_NONNUMERIC format, 
+non-quoted values are converted into float values.
+
+"""
+import csv 
+
+with open('my_file.csv', dialect='excel') as fh:
+    
+    fh.close()
