87b6dcc0fc9a1851a21b886163bd1c15c8fdcb2a
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2020-05-17 13:02:37 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: cfdd340e2b3e3a3ff6565ab4d72d9850d2ae60e9

---
+++ python3/02_Basics/01_Arithmetic_Operations/k_abs_function.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+"""
+Builtin function, to return the absolute value.
+
+If a is positive real integer, 
+    abs(a)  = a
+    abs(-a) = a
+    abs(a+bj)  is equal to math.sqrt(pow(a,2)+ pow(b,2))
+"""
+
+print("abs(3)          = ", abs(3))
+print("abs(-3)         = ", abs(-3))
+print("abs(-3234.34234)= ", abs(-3234.34234))
+print("abs(0.00000001) = ", abs(0.00000001))
+
+print("abs(-3 + 4j)    = ", abs(-3 + 4j))  # 5
+print("abs(-3 + 4.0j)  = ", abs(-3 + 4.0j))  # 5.0
+
+# loading a module (library) to use
+import math
+
+print(math.sqrt(pow(-3, 2) + pow(4, 2)))  # 5
+print(math.sqrt((-3) ** 2 + 4 ** 2))  # 5
+
+print(math.sqrt(-3 ** 2 + 4 ** 2))  # 2.64575131106- PEMDAS
