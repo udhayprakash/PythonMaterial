921bc69cbfd654ef62dc89b90ff1ebcd857b4389
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2020-06-05 01:59:06 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 03289d60dadcdeaa10892b46adfeff36fc5b0494

--- python3/16_Web_Services/03_web_scrapping/webScraping.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-
-htmlfile = urllib.request.urlopen("http://google.com")
-
-htmltext = htmlfile.read()
-
-print(htmltext)
--- python3/16_Web_Services/03_web_scrapping/webScraping3.py
+++
@@ -1,3 +0,0 @@
-import urllib.error
-
-htmlfile = urllib.openurl("http://finance.yahoo.com/q?s=AAPL")
