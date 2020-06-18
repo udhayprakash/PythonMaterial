68a743460ac7a0b0f29471b9a2a2fdeac7dcbab4
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2020-06-18 10:10:08 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: bc30df59d03726926c6fca4e91c0a3dbd58cedb4

--- python3/08_Decorators/13_phone_number_formatter.py
+++ python3/08_Decorators/13_phone_number_formatter.py
@@ -1,5 +1,6 @@
-
-"""
+def wrapper(f):
+    def fun(l):
+        """
             7895462130      10
             9195969878      10
             9100256236      10
@@ -8,13 +9,23 @@
             919875641230    12
             +919875641230   13
             +919593621456   13
-"""
-def phone_number_formatter(func):
-    def wrapper(*args, **kwargs):
-        pass 
+        """
+        new_list = []
+        for ech_num in l:
+            if len(ech_num) == 10:
+                new_list.append('+91 ' + ech_num[:5] + ' ' + ech_num[5:])
+            elif len(ech_num) == 11:
+                new_list.append('+91 ' + ech_num[1:][:5] + ' ' + ech_num[1:][5:])
+            elif len(ech_num) == 12:
+                new_list.append('+91 ' + ech_num[2:][:5] + ' ' + ech_num[2:][5:])
+            elif len(ech_num) == 13:
+                new_list.append('+91 ' + ech_num[3:][:5] + ' ' + ech_num[3:][5:])
+        f(new_list)
 
-    return wrapper 
+    return fun
 
+
+@wrapper
 def sort_phone(l):
     print(*sorted(l), sep='\n')
 
@@ -29,5 +40,3 @@ if __name__ == '__main__':
          '9100256236',
          '+919593621456']
     sort_phone(l)
-
-# Assignment is to complete this decorator
