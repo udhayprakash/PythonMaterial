54d40aa059a68495943c30393c749c53c5696f68
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2020-06-19 04:48:54 +0530

    exception & debug hooks
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 8ef00c95e57c32c21869579b26ea1a7c76b5956c

---
+++ python3/05_Debugging/04_example.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+"""
+Purpose: breakpoint hook 
+    - sys.breakpointhook 
+        - Gets called when breakpoint is called
+        - works only with builtin breakpoint() function
+        - Doesnt work with pdb/ipdb modules
+"""
+import sys 
+
+def hello():
+    print('Breakpoint got activated')
+
+sys.breakpointhook = hello
+
+
+numbers = range(0, 15)
+
+breakpoint()
+# import pdb;pdb.set_trace()
+# import ipdb;ipdb.set_trace()
+
+for each_num in numbers:
+    if each_num % 2 == 0:
+        print(each_num, end=' ')
