7f406a7145171a9ad8fdd7174a3a939868d3f92a
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2020-06-26 01:06:46 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 852ca35ec9ce79d0278b0740849937e989ab3ba0

--- python3/10_Modules/07_getpass/01_getpass_ex.py
+++
@@ -1,9 +0,0 @@
-from getpass import getpass, getuser
-
-user_name = input('User Name:')
-pass_word = getpass('Password:')
-
-print("user_name -", user_name)
-print("pass_word -", pass_word)
-
-print('getuser()', getuser())
--- python3/10_Modules/08_time_related/b_timeit_module/a_timeit_module.py
+++ python3/10_Modules/08_time_related/b_timeit_module/a_timeit_module.py
@@ -1,13 +1,13 @@
 """
 In commandline, 
-    #python -m timeit "'-'.join(str(n) for n in range(10))"
-    20000 loops, best of 5: 12.2 usec per loop
+    ~python -m timeit "'-'.join(str(n) for n in range(10))"
+    100000 loops, best of 5: 2.48 usec per loop
 
-    #python -m timeit "'-'.join([str(n) for n in range(10)])"
-    20000 loops, best of 5: 10.1 usec per loop
+    ~python -m timeit "'-'.join(str(n) for n in range(10))"
+    100000 loops, best of 5: 2.94 usec per loop
 
-    #python -m timeit "'-'.join(map(str, range(10)))"
-    20000 loops, best of 5: 9.05 usec per loop
+    ~python -m timeit "'-'.join(map(str, range(10)))"
+    200000 loops, best of 5: 2.23 usec per loop
 
 SYNTAX:
 python -m timeit [-n N] [-r N] [-u U] [-s S] [-h] [statement ...]
@@ -17,30 +17,44 @@ python -m timeit [-n N] [-r N] [-u U] [-s S] [-h] [statement ...]
 -s , --setup   : statement to be executed once initially (default pass)
 -u , --unit    : specify a time unit for timer output; can select nsec, usec, msec, or sec
 
+~python -m timeit -n 20 "'-'.join(map(str, range(10)))"
+20 loops, best of 5: 2.72 usec per loop
+:0: UserWarning: The test results are likely unreliable. The worst time (12.2 usec) was more than four times slower than the best time (2.72 usec).
 
-#python -m timeit -n 20 "'-'.join(map(str, range(10)))"
-20 loops, best of 5: 8.78 usec per loop
+~python -m timeit -n 20 "'-'.join(map(str, range(10)))"
+20 loops, best of 5: 2.43 usec per loop
 
-#python -m timeit -n 20 -r 15 "'-'.join(map(str, range(10)))"
-20 loops, best of 15: 8.88 usec per loop
+~python -m timeit -n 20 -r 15 "'-'.join(map(str, range(10)))"
+20 loops, best of 15: 1.75 usec per loop
 
-#python -m timeit -n 20 -r 15 -u msec "'-'.join(map(str, range(10)))"
-20 loops, best of 15: 0.00783 msec per loop
+~python -m timeit -n 20 -r 15 -u msec "'-'.join(map(str, range(10)))"
+20 loops, best of 15: 0.00168 msec per loop
 
-#python -m timeit -n 20 -r 15 -u sec "'-'.join(map(str, range(10)))"
-20 loops, best of 15: 7.8e-06 sec per loop
+~python -m timeit -n 20 -r 15 -u sec "'-'.join(map(str, range(10)))"
+20 loops, best of 15: 1.72e-06 sec per loop
 
-#python -m timeit -n 20 -r 15 -u sec -v "'-'.join(map(str, range(10)))"
-raw times: 0.00035 sec, 0.000332 sec, 0.000336 sec, 0.000336 sec, 0.000335
-sec, 0.000335 sec, 0.000654 sec, 0.000178 sec, 0.000176 sec, 0.000177 sec,
-0.000176 sec, 0.000176 sec, 0.000176 sec, 0.000175 sec, 0.000176 sec
+~python -m timeit -n 20 -r 15 -u sec -v "'-'.join(map(str, range(10)))"
+raw times: 6.91e-05 sec, 6.67e-05 sec, 6.58e-05 sec, 6.78e-05 sec, 9.45e-05 sec, 5.89e-05 sec, 6.74e-05 sec, 6.73e-05 sec, 6.66e-05 sec, 6.13e-05 sec, 6.02e-05 sec, 0.000449 
+sec, 7.09e-05 sec, 8.62e-05 sec, 5.8e-05 sec
 
-20 loops, best of 15: 8.75e-06 sec per loop
+20 loops, best of 15: 2.9e-06 sec per loop
+:0: UserWarning: The test results are likely unreliable. The worst time (2.24e-05 sec) 
+was more than four times slower than the best time (2.9e-06 sec).
+
+~python -m timeit -n 20 -r 15 -u sec -v "'-'.join(map(str, range(10)))"
+raw times: 3.9e-05 sec, 3.49e-05 sec, 3.46e-05 sec, 3.46e-05 sec, 3.55e-05 sec, 3.47e-05 sec, 3.46e-05 sec, 3.47e-05 sec, 3.45e-05 sec, 3.47e-05 sec, 3.46e-05 sec, 3.47e-05 sec, 3.46e-05 sec, 3.47e-05 sec, 3.44e-05 sec
+
+20 loops, best of 15: 1.72e-06 sec per loop
+
+~python -m timeit -n 20 -r 5 -u usec -v "'-'.join(map(str, range(10)))"
+raw times: 38.7 usec, 40.4 usec, 50.5 usec, 34.6 usec, 34.5 usec
+
+20 loops, best of 5: 1.72 usec per loop
 """
 import timeit
 
-print(timeit.timeit('"-".join(str(n) for n in range(10))', number=10000))
-print(timeit.timeit('"-".join([str(n) for n in range(10)])', number=10000))
+`print(timeit.timeit('"-".join(str(n) for n in range(10))', number=10000))
+`print(timeit.timeit('"-".join([str(n) for n in range(10)])', number=10000))
 print(timeit.timeit('"-".join(map(str, range(10)))', number=10000))
 
 print(timeit.timeit(lambda: "-".join(map(str, range(10))), number=10000))
