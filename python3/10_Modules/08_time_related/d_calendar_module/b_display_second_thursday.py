0ec22285f8857a8503f2d48e8b835878b831bd57
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2020-06-26 01:29:58 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: fb6c0c237d893c62a7dbe04e892260020a02e25e

---
+++ python3/10_Modules/08_time_related/d_calendar_module/b_display_second_thursday.py
@@ -0,0 +1,25 @@
+import calendar
+import sys
+
+year = int(sys.argv[1])
+
+# Show every month
+for month in range(1, 13):
+
+    # Compute the dates for each week that overlaps the month
+    c = calendar.monthcalendar(year, month)
+    first_week = c[0]
+    second_week = c[1]
+    third_week = c[2]
+
+    # If there is a Thursday in the first week,
+    # the second Thursday is # in the second week.
+    # Otherwise, the second Thursday must be in
+    # the third week.
+    if first_week[calendar.THURSDAY]:
+        meeting_date = second_week[calendar.THURSDAY]
+    else:
+        meeting_date = third_week[calendar.THURSDAY]
+
+    print('{:>3}: {:>2}'.format(calendar.month_abbr[month],
+                                meeting_date))
