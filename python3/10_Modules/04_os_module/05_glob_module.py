caf71c952ffddcf8597b363ac18c5da29859584d
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2020-08-21 17:13:49 +0530

    glob module
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
    
    Former-commit-id: 82a4185f30c33b93476790a26016a2e3779b46f5

---
+++ python3/10_Modules/04_os_module/05_glob_module.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Purpose: File Pattern Matching using glob module 
+"""
+import sys
+import glob
+import os
+
+print(f'{sys.exec_prefix =}')
+os.chdir(sys.exec_prefix)
+
+print('\n Current Directory')
+text_files = glob.glob('*.txt')
+for each_txt_file in text_files:
+    print('\t', each_txt_file)
+
+print('\n One Level Deep')
+text_files = glob.glob('*/*.txt')
+for each_txt_file in text_files:
+    print('\t', each_txt_file)
+
+print('\n Two Levels Deep')
+text_files = glob.glob('*/*/*.txt')
+for each_txt_file in text_files:
+    print('\t', each_txt_file)
+
+print('\n Three Levels Deep')
+text_files = glob.glob('*/*/*/*.txt')
+for each_txt_file in text_files:
+    print('\t', each_txt_file)
