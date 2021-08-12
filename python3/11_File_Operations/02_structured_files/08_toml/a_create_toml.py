689c8c89603752c27e6f94e2ed163e977a4588e6
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2021-08-12 05:39:06 +0530

    toml files
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ python3/11_File_Operations/02_structured_files/08_toml/a_create_toml.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Purpose: TOML - Tom's Obvious, Minimal Language.
+
+
+    https://toml.io/en/
+    https://github.com/toml-lang/toml
+
+pip install toml
+
+"""
+import toml
+
+print(dir(toml))
+print()
+
+# toml file to python dict 
+with open('sample.toml', 'r') as fh:
+    parsed_toml_dict = toml.load(fh)
+    print(parsed_toml_dict)
+
+print()
+# python dict to toml string
+toml_string = toml.dumps(parsed_toml_dict)
+print(toml_string)
+
+print()
+# python dict to toml file
+with open('new_file.toml', 'w') as fh:
+    toml.dump(parsed_toml_dict, fh)
