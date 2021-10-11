24e5d7bb795ba003d2e45e49370a9934579edd8d
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2021-10-11 20:41:03 +0530

    Update 04_immutability.py

--- python3/06_Collections/02_Tuples/04_immutability.py
+++ python3/06_Collections/02_Tuples/04_immutability.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """
 Purpose: Tuples are immutable
             - They doesnt support in-object changes
@@ -7,13 +7,17 @@ Purpose: Tuples are immutable
 mytuple = (1, 2, 3)
 print('mytuple', mytuple, id(mytuple))
 
+#  Indexing
+print(f'{mytuple[2] =}')
+
+# updating an element in tuple
 try:
     mytuple[2] = '2.2222'
 except TypeError as ex:
     print(ex)
     print('tuple objects are not editable')
 
-# Overwriting 
+# Overwriting
 print('\noverwriting (re-assigning)')
-mytuple = (1,'2.2222', 3)
+mytuple = (1, '2.2222', 3)
 print('mytuple', mytuple, id(mytuple))
