2623084031a6ab282da8883e02f57de88b58015d
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2021-11-13 01:55:12 +0530

    updated

--- python3/10_Modules/02_sys/d_sys_get_size_of.py
+++ python3/10_Modules/02_sys/d_sys_get_size_of.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """
-Purpose: sys module 
+Purpose: sys module
 """
-import sys 
+import sys
 
-my_int = 123 # int 
-print(f'\n{my_int.__sizeof__()  =}') # 28
+my_int = 123  # int
+print(f'\n{my_int.__sizeof__()  =}')  # 28
 print(f'{sys.getsizeof(my_int)=}')
 
-my_int = 12323123231232312323 # int 
-print(f'\n{my_int.__sizeof__()  =}') # 36
+my_int = 12323123231232312323  # int
+print(f'\n{my_int.__sizeof__()  =}')  # 36
 print(f'{sys.getsizeof(my_int)=}')
 
-my_float = 123.23 # float 
-print(f'\n{my_float.__sizeof__()  =}') # 24
+my_float = 123.23  # float
+print(f'\n{my_float.__sizeof__()  =}')  # 24
 print(f'{sys.getsizeof(my_float)=}')
 
-my_none = None # None Type
+my_none = None  # None Type
 print(f'\n{my_none.__sizeof__()  =}')
 print(f'{sys.getsizeof(my_none)=}')
 
@@ -24,21 +24,23 @@ my_bool = True  # bool
 print(f'\n{my_bool.__sizeof__()  =}')
 print(f'{sys.getsizeof(my_bool)=}')
 
-
-my_str = 'Python' # str
+my_str = 'Python'  # str
 print(f'\n{my_str.__sizeof__()   =}')
 print(f'{sys.getsizeof(my_str) =}')
 
 # ----------------
-my_list = [12, 123, 23] # list
-print(f'\n{my_list.__sizeof__()   =}') # 104
-print(f'{sys.getsizeof(my_list) =}')   # 120
+my_list = [12, 123, 23]  # list
+print(f'\n{my_list.__sizeof__()   =}')  # 104
+print(f'{sys.getsizeof(my_list) =}')  # 120
 
+my_tuple = (12, 123, 23)  # tuple
+print(f'\n{my_tuple.__sizeof__()   =}')  # 48
+print(f'{sys.getsizeof(my_tuple) =}')  # 64
 
-my_tuple = (12, 123, 23) # tuple
-print(f'\n{my_tuple.__sizeof__()   =}')
-print(f'{sys.getsizeof(my_tuple) =}')
+my_set = {12, 123, 23}  # set
+print(f'\n{my_set.__sizeof__()   =}')  # 200
+print(f'{sys.getsizeof(my_set) =}')  # 216
 
-my_set = {12, 123, 23} # set
-print(f'\n{my_set.__sizeof__()   =}')
-print(f'{sys.getsizeof(my_set) =}')
+my_dict = {12: 123, 13: 23}  # dict
+print(f'\n{my_dict.__sizeof__()   =}')  # 216
+print(f'{sys.getsizeof(my_dict) =}')  # 232
