86e0e8e05f87dad5d3181e1089decc1af7602471
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2021-12-26 13:02:51 +0530

    singleton

--- python3/13_OOP/34_singleton_design_pattern.py
+++ python3/13_OOP/34_singleton_design_pattern.py
@@ -10,6 +10,7 @@ from pprint import pprint
 # baby born   => __new__
 # baby named  => __init__
 
+
 class Logger(object):
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, '_logger'):
@@ -27,3 +28,26 @@ l2 = Logger()
 print(id(l1), id(l2))
 
 assert l1 is l2
+
+# ------------------------------------
+
+
+class Base(object):
+    __singleton = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls.__singleton:
+            cls.__singleton = super(Base, cls).__new__(cls, *args, **kwargs)
+        return cls.__singleton
+
+
+class MyClass(Base):
+    def __init__(self) -> None:
+        Base.__init__(self)
+
+
+c1 = MyClass()
+print(c1)  # <__main__.MyClass object at 0x00000244B095EFE0>
+
+c2 = MyClass()
+print(c2)  # <__main__.MyClass object at 0x00000244B095EFE0>
