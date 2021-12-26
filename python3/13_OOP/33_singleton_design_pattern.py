52cf609a5544f1bd8bba1ddee4a086289626352c
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2021-12-26 14:15:05 +0530

    updated

--- python3/13_OOP/33_singleton_design_pattern.py
+++ python3/13_OOP/33_singleton_design_pattern.py
@@ -23,3 +23,29 @@ print()
 
 # Doesnt allow to create second instance
 d = Singleton()
+
+# ----
+
+
+# Question: __new__ vs __init__
+# baby born   => __new__
+# baby named  => __init__
+
+
+class Logger(object):
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_logger'):
+            cls._logger = super(Logger, cls).__new__(cls, *args, **kwargs)
+        return cls._logger
+
+
+l1 = Logger()
+pprint(vars(Logger))
+
+print(f'l1:{l1}')
+print(vars(l1))
+
+l2 = Logger()
+print(id(l1), id(l2))
+
+assert l1 is l2
