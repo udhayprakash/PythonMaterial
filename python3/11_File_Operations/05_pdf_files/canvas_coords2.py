d7bf73b48a7a1254680c1f9b0b9db73249fd67ca
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2022-01-10 15:45:03 +0530

    updated

---
+++ python3/11_File_Operations/05_pdf_files/canvas_coords2.py
@@ -0,0 +1,12 @@
+from reportlab.pdfgen import canvas    
+from reportlab.lib.units import mm
+
+def coord(x, y, unit=1):
+    x, y = x * unit, y * unit
+    return x, y
+
+c = canvas.Canvas("hello2.pdf", bottomup=0)
+
+c.drawString(*coord(15, 20, mm), text="Welcome to Reportlab!")
+c.showPage()
+c.save()
