eadf86c1b156071dce5fb73bb97ff0dac7447d80
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2022-01-14 05:43:29 +0530

    updated

---
+++ python3/11_File_Operations/02_structured_files/05_xls_files/openpyxl_module/j_csv_to_excel.py
@@ -0,0 +1,20 @@
+import csv
+import openpyxl
+
+
+def csv_to_excel(csv_file, excel_file):
+    csv_data = []
+    with open(csv_file) as file_obj:
+        reader = csv.reader(file_obj)
+        for row in reader:
+            csv_data.append(row)
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    for row in csv_data:
+        sheet.append(row)
+    workbook.save(excel_file)
+
+
+if __name__ == "__main__":
+    import os
+    csv_to_excel("../../04_csv/my_file.csv", "my_file.xlsx")
---
+++ python3/11_File_Operations/03_image_files/14_draw_shapes.py
@@ -0,0 +1,15 @@
+from PIL import Image, ImageDraw
+
+def rectangle(output_path):
+    image = Image.new("RGB", (400, 400), "green")
+    draw = ImageDraw.Draw(image)
+    # Draw a regular rectangle
+    draw.rectangle((200, 100, 300, 200), fill="red")
+    # Draw a rounded rectangles
+    draw.rounded_rectangle((50, 50, 150, 150), fill="blue", outline="yellow",
+                           width=3, radius=7)
+    image.save(output_path)
+    
+    
+if __name__ == "__main__":
+    rectangle("images/rounded_rectangle.jpg")
