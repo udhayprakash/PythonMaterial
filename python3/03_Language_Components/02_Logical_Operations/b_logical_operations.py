bd113c1dbd9197ef7c75387a4407e893586a184c
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2022-02-21 19:37:26 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

--- python3/03_Language_Components/02_Logical_Operations/b_logical_operations.py
+++ python3/03_Language_Components/02_Logical_Operations/b_logical_operations.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
 Purpose: Logical Operations
+    Chaining Comparsion(Relational) Operators
 """
 print('23 <73 <45', 23 < 73 < 45)
 #   23 <73  and     73 <45
@@ -17,3 +18,7 @@ print('89 <73 <99< 999< 0', 89 < 73 < 99 < 999 < 0)
 print('1<2<3<4<5', 1 < 2 < 3 < 4 < 5)
 # 1 < 2 and 2 < 3 and 3 < 4 and 4 < 5
 # True  and True  and True  and True = True
+
+print(f'{11 == 11 == 11 =}') # True
+print(f'{11 != 12 != 13 =}') # False
+print(f'{11 != 12 != 11 =}') # True
