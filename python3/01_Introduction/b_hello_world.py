270d110b824d62fa73c1c4efa0769a142979711f
udhay Prakash <uday3prakash@gmail.com>
2023-02-11 13:45:25 +0000

    updated

--- python3/01_Introduction/b_hello_world.py
+++ python3/01_Introduction/b_hello_world.py
@@ -4,6 +4,9 @@ Purpose: Basic PEP-8 guidelines and
        shebang line
     PEP - Python Enhancement Prosopal
     PEP 8 - coding style guide
+
+    https://peps.python.org/pep-0008/
+
 """
 # print as a statement in python 2
 # print "Hello world!"
