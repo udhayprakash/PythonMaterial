f875d5b480e2bc9c020e55cf79e8cccb775ca585
udhay Prakash <uday3prakash@gmail.com>
2023-02-14 16:14:37 +0000

    updated

--- python3/02_Basics/01_Arithmetic_Operations/i_feet_to_cms_conversion.py
+++ python3/02_Basics/01_Arithmetic_Operations/i_feet_to_cms_conversion.py
@@ -4,6 +4,10 @@ Purpose: Feet to centimetres conversion
 
     1 foot = 12 inches
     1 inch = 2.54 centimetres
+
+Step 1: Get feet values in runtime
+Step 2: compute  from feet to cms
+sTep 3: display the result
 """
 print("How tall are you in feet and inches?")
 feet = int(input("Feet:"))  # 5
