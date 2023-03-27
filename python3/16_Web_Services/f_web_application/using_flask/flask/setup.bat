91098edacb2974b6f15c49f0437a46c18dc947fd
udhay Prakash <uday3prakash@gmail.com>
2023-03-27 08:43:17 +0100

    updated

--- python3/16_Web_Services/f_web_application/using_flask/flask/setup.bat
+++
@@ -1,4 +0,0 @@
-virtualenv venv
-venv/Scripts/activate.bat
-pip install flask flask-jsonpify flask-sqlalchemy flask-restful
-pip freeze > requirements.txt
--- python3/16_Web_Services/f_web_application/using_flask/flask/setup.sh
+++
@@ -1,4 +0,0 @@
-$ virtualenv venv
-$ source venv/bin/activate
-$ pip install flask flask-jsonpify flask-sqlalchemy flask-restful
-$ pip freeze
