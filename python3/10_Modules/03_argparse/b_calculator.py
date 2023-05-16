061a9e2f394aa9277c8bfe12a4145821c1914180
udhay Prakash <uday3prakash@gmail.com>
2023-05-16 01:53:11 +0100

    updated

--- python3/10_Modules/03_argparse/b_calculator.py
+++ python3/10_Modules/03_argparse/b_calculator.py
@@ -22,8 +22,9 @@ parser = argparse.ArgumentParser(description="Script to add two/three numbers")
 
 # Step 2: add arguments to parser object
 parser.add_argument("-num1", help="First value", type=int, default=0)
-parser.add_argument("-num2", help="Second value", type=int, default=0)
-parser.add_argument("-num3", help="optional third value", type=float, default=1)
+parser.add_argument("-num2", help="Second Value", type=int, default=0)
+parser.add_argument("-num3", help="optonal value", type=float, default=0.0)
+
 
 # Step 3: Built parser objects and extract values
 args = parser.parse_args()
@@ -33,8 +34,14 @@ num2 = args.num2
 print(f" {type(num1) =} {num1 = }")
 print(f" {type(num2) =} {num2 = }")
 print(f" {type(args.num3) =} {args.num3 = }")
+print()
 
 # Default Values are None for the args
 print(f"{addition(num1, num2)                 =}")
 print(f"{subtraction(num1, num2)              =}")
 print(f"{multiplication(num1, num2,args.num3) =}")
+
+# python b_calculator.py
+# python b_calculator.py -num1 10
+# python b_calculator.py -num1 10 -num2 20
+# python b_calculator.py -num1 10 -num2 20 -num3 30
--- python3/10_Modules/03_argparse/c_arg_parse.py
+++ python3/10_Modules/03_argparse/c_arg_parse.py
@@ -5,10 +5,24 @@ Purpose: argparse
 import argparse
 
 my_parser = argparse.ArgumentParser()
-# my_parser.add_argument('--input', action='store',  nargs=3)
-my_parser.add_argument("--input", action="store", type=int, nargs=3)
-# NOTE: expected three values mandatorily
+
+
+# my_parser.add_argument("--inputvalues", action="store", nargs=3)
+my_parser.add_argument("--inputvalues", action="store", nargs=3, type=int)
+
 
 args = my_parser.parse_args()
-print(f"{args.input      =}")
-print(f"{type(args.input)=}")  # list
+
+print(f"{args.inputvalues =}")
+print(f"{type(args.inputvalues)=}")  # list
+
+
+"""
+python c_arg_parse.py
+args.inputvalues =None
+
+python c_arg_parse.py --inputvalues 12 13 14
+args.inputvalues =[12, 13, 14]
+type(args.inputvalues)=<class 'list'>
+
+"""
