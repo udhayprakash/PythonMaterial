1517699b118fc54ad9441822cdd238892cbf103d
udhay Prakash <uday3prakash@gmail.com>
2023-06-08 08:19:26 +0100

    updated

--- python3/16_Web_Services/a_web_scraping/webScraping.py
+++
@@ -1,9 +0,0 @@
-import urllib.error
-import urllib.parse
-import urllib.request
-
-htmlfile = urllib.request.urlopen("http://google.com")
-
-htmltext = htmlfile.read()
-
-print(htmltext)
--- python3/16_Web_Services/a_web_scraping/webScraping3.py
+++
@@ -1,5 +0,0 @@
-import urllib.error
-import urllib.parse
-import urllib.request
-
-htmlfile = urllib.openurl("http://finance.yahoo.com/q?s=AAPL")
