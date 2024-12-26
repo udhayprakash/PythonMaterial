564981c542919bc6779bf7e63787e5b999db66c8
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-26 14:28:46 +0000

    Update 01_subprocess_ex.py

--- python3/10_Modules/06_subprocess/01_subprocess_ex.py
+++ python3/10_Modules/06_subprocess/01_subprocess_ex.py
@@ -1,27 +1,28 @@
 import os
-from subprocess import PIPE, Popen, call
-from sys import exit, platform
+import subprocess
+import sys
 
 # subprocess.Popen is more general than subprocess.call.
 # Popen doesn't block, allowing you to interact with the
 # process while it's running, or continue with other things
 # in your Python program; whereas call does block.
 
-if platform in ["linux", "linux2", "darwin"]:
-    os.system("ls -l /usr/bin/python")
-    call("ls -l /usr/bin/python", shell=True)
-    myprocess = Popen("ls -l /usr/bin/python", shell=True, stdin=PIPE, stdout=PIPE)
+if sys.platform in ("linux", "linux2", "darwin"):
+    cmd = "ifconfig"
+    os.system(cmd)
+    subprocess.call(cmd, shell=True)
+    myprocess = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     output, err = myprocess.communicate()
     print("output==============\n", output)
     print("err=================\n", err)
-elif platform == "win32":
+elif sys.platform == "win32":
     # print(os.system('dir /x C:\Python27'))
     # print(call('dir /x C:\Python27', shell=True))
 
-    myprocess = Popen(r"diras /x C:\Python27", shell=True, stderr=PIPE, stdout=PIPE)
+    myprocess = subprocess.Popen(r"diras /x C:\Python27", shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     output, err = myprocess.communicate()
     print("output==============\n", output.decode("utf-8"))
     print("err=================\n", err.decode("utf-8"))
 else:
-    print("unhandled platform :", platform)
+    print("unhandled platform :", sys.platform)
     exit(1)
