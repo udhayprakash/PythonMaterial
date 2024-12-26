0580afe58d08a9949b4819ee9ffacf90d9948d83
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-26 14:15:10 +0000

    Update 01_copy_file.py

--- python3/10_Modules/05_shutil/01_copy_file.py
+++ python3/10_Modules/05_shutil/01_copy_file.py
@@ -1,11 +1,33 @@
-# -*- coding: utf-8 -*-
 """
 Purpose: To copy a file from one directory, to another
 """
+import os
+import shutil
+
+# print(dir(shutil))
+
+
+'''
+ 'copy', 'copy2', 'copyfile', 'copyfileobj', 'copymode', 'copystat',
+ 'copytree', 'disk_usage', 'errno', 'fnmatch', 'get_archive_formats'
+ , 'get_terminal_size', 'get_unpack_formats', 'ignore_patterns', 'make_archive', 'move', 'nt', 'os', 'posix', 'register_archive_format', 'register_unpack_format', 'rmtree', 'stat', 'sys', 'unpack_archive', 'unregister_archive_format', 'unregister_unpack_format', 'warnings', 'which
+
+'''
+
+python_path = shutil.which("python")
+print(python_path)
+
+
+ls_path = shutil.which("ls")
+print(ls_path)
+
+
 
 import os
 import shutil
 
+
+# Purpose: To copy a file from one directory, to another
 
 def copy_file(src_dir, dest_dir, filename):
     source_path = os.path.join(src_dir, filename)
@@ -14,6 +36,7 @@ def copy_file(src_dir, dest_dir, filename):
     if os.path.exists(dest_path):
         print("file already exists. So, deleting")
         os.remove(dest_path)
+    os.makedirs(dest_dir)
 
     shutil.copyfile(source_path, dest_path)
 
