747b13225edccc91552e970328938b0382bd5cb4
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-30 15:57:01 +0000

    Update b_write_xml.py

--- python3/11_File_Operations/02_structured_files/02_xml/01_xml/b_write_xml.py
+++ python3/11_File_Operations/02_structured_files/02_xml/01_xml/b_write_xml.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 """
 Purpose: write xml file using lxml module
 """
@@ -8,8 +7,10 @@ except ModuleNotFoundError as ex:
     print(repr(ex))
     from os import system
 
-    system("pip install lxml --user")
+    system("pip install lxml")
     from lxml import etree
+
+
 
 # creating the XML
 root = etree.Element("root")
@@ -17,14 +18,31 @@ root = etree.Element("root")
 child1 = etree.Element("child1")
 root.append(child1)
 
+
 # another child with text
 child2 = etree.Element("child2")
 child2.text = "some text"
 
+
 root.append(child2)
 # child1.append(child2)
 
-# pretty string
-s = etree.tostring(root, pretty_print=True)
-# print(s)
-print(s.decode("utf-8"))
+
+xml_Str = etree.tostring(root)
+print(xml_Str)
+# b'<root><child1/><child2>some text</child2></root>'
+
+xml_Str = etree.tostring(root, pretty_print=True)
+print(xml_Str.decode('utf-8'))
+
+# <root>
+#   <child1/>
+#   <child2>some text</child2>
+# </root>
+
+
+# <root>
+#   <child1>
+#     <child2>some text</child2>
+#   </child1>
+# </root>
