0b601da891a8eaa52cb399ec9547c6009ef89ed4
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-30 16:05:31 +0000

    Update c_parse_xml.py

--- python3/11_File_Operations/02_structured_files/02_xml/01_xml/c_parse_xml.py
+++ python3/11_File_Operations/02_structured_files/02_xml/01_xml/c_parse_xml.py
@@ -1,27 +1,24 @@
-#!/usr/bin/python3
 """
 Purpose: Reading(Parsing) XML
 """
-from pprint import pprint
-from xml.etree.ElementTree import parse
 
-tree = parse("books.xml")
+import xml.etree.ElementTree as ET
+from pprint import pp
+
+tree = ET.parse("books.xml")
+
+# print(dir(tree))
 
 # To check for presence of a particular tag in xml file
-# print(f"{tree.findall('book') =}")
-print()
+# print(tree.find('book'))
+# print(tree.findall('book'))
+# print()
 
 books = {}
-for each in tree.findall("book"):
-    isbn = each.attrib["isbn"]
-    for ech_sub in each.findall("title"):
-        book_title = ech_sub.text
+for each in tree.findall('book'):
+    isbn= each.attrib['isbn']
+    for each_sub in each.findall('title'):
+        book_title = each_sub.text
         books[isbn] = book_title
-# print(dir(each))
 
-pprint(books)
-
-# Assignment: Enhance it by changing the values of books dict
-# as a tuple containing book_title, author and date
-
-{"0-596-00128-2": ("Python & XML", "Jones, Drake", "December 2001")}
+pp(books)
