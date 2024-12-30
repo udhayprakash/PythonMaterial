7ca7e2886edda879f25354ff21b241acbc0edbcd
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-30 16:19:43 +0000

    Update d_parsing_html.py

--- python3/11_File_Operations/02_structured_files/02_xml/02_html/d_parsing_html.py
+++ python3/11_File_Operations/02_structured_files/02_xml/02_html/d_parsing_html.py
@@ -1,10 +1,5 @@
-#!/usr/bin/python
-"""
-Purpose: Parsing html using lxml
-"""
 from io import StringIO
 
-import lxml.etree
 from lxml import etree
 
 # Feeding RAW XML for Serialisation
@@ -12,23 +7,27 @@ html = etree.XML(
     "<html><head>Head of HTML</head><title>I am the title!</title><body>Here is the body</body></html>"
 )
 print(etree.tostring(html, pretty_print=True).decode("utf-8"))
+print()
+
 
 
 html = etree.XML(
     "<html><head>Head of HTML</head><title>I am the title!</title><body>Here is the body</body></html>"
 )
 print(etree.tostring(html, pretty_print=True, xml_declaration=True).decode("utf-8"))
+print()
 
 
 # The parse() function can be used to parse from files and file-like objects
 title = StringIO("<title>Title Here</title>")
-tree = etree.parse(title, parser=lxml.etree.XMLParser(resolve_entities=False))
+tree = etree.parse(title, parser=etree.XMLParser(resolve_entities=False))
 print(etree.tostring(tree).decode("utf-8"))
+
 
 
 # The fromstring() function can be used to parse Strings:
 title = "<title>Title Here</title>"
-root = etree.fromstring(title, parser=lxml.etree.XMLParser(resolve_entities=False))
+root = etree.fromstring(title, parser=etree.XMLParser(resolve_entities=False))
 print(root.tag)
 print(etree.tostring(root).decode("utf-8"))
 
