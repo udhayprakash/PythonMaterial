819cfe562c38b1b26d33f6da2d6d8d34994ce1f8
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-30 16:29:48 +0000

    Update b_read_cv.py

--- python3/11_File_Operations/02_structured_files/03_csv/01_csv/b_read_cv.py
+++ python3/11_File_Operations/02_structured_files/03_csv/01_csv/b_read_cv.py
@@ -1,28 +1,31 @@
-#!/usr/bin/python
 """
 Purpose: Reading(Parsing) csv, using unstructure file ops
 """
 # Method 1
-with open("first.csv", mode="r") as fh:
+with open("my_file.csv", mode="r") as fh:
     file_content = fh.read()
 
-    names = []
-    for each_line in file_content.splitlines()[1:]:
-        # print(each_line.split(","))
-        name = each_line.split(",")[1]
-        names.append(name)
 
-print(f"{names =}")
+# print(file_content.split())
+# print(file_content.splitlines())
+
+names = []
+for each_line in file_content.splitlines()[1:]:
+    name = each_line.split(',')[1]
+    names.append(name)
+
+print(f"{names = }")
+# names = ['akhila Bhopal', 'neha', 'hiral']
+
 
 
 # Method 2
-with open("first.csv", mode="r") as fh:
+with open("my_file.csv", mode="r") as fh:
     file_content = fh.readlines()
 
-    names = []
-    for each_line in file_content[1:]:
-        # print(each_line.split(","))
-        name = each_line.split(",")[1]
-        names.append(name)
+names = []
+for each_line in file_content[1:]:
+    name = each_line.split(',')[1]
+    names.append(name)
 
-print(f"{names =}")
+print(f"{names = }")
